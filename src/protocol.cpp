faddc2af39ca833be90bb400f64403d380d3b8c0
Volker Fischer <corrados@users.noreply.github.com>
2006-03-06 17:17:55 +0000

    test

--- src/protocol.cpp
+++ src/protocol.cpp
@@ -170,8 +170,8 @@ bool CProtocol::ParseMessage ( const CVector<unsigned char>& vecbyData,
 /*
 	return code: true -> ok; false -> error
 */
-//	Mutex.lock();
-//	{
+	Mutex.lock();
+	{
 		int					iRecCounter, iRecID, iData;
 		unsigned int		iPos;
 		CVector<uint8_t>	vecData;
@@ -250,8 +250,8 @@ for ( int i = 0; i < iNumBytes; i++ ) {
 		{
 			return false; // return error code
 		}
-//	}
-//	Mutex.unlock();
+	}
+	Mutex.unlock();
 }
 
 void CProtocol::CreateJitBufMes ( const int iJitBufSize )

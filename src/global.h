174ad54c4fd860a3e5155a48c1910e7ebf6db431
Volker Fischer <corrados@users.noreply.github.com>
2009-03-13 15:39:25 +0000

    next cvs version

--- src/global.h
+++ src/global.h
@@ -43,7 +43,7 @@
 
 // version and application name (always use this version)
 #undef VERSION
-#define VERSION                         "2.2.0"
+#define VERSION                         "2.2.1cvs"
 #define APP_NAME                        "llcon"
 
 // file name for logging file

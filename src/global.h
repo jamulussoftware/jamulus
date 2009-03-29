7f9de079b04ce726c6e323489bf5f77695f5e461
Volker Fischer <corrados@users.noreply.github.com>
2009-03-29 16:50:29 +0000

    new version

--- src/global.h
+++ src/global.h
@@ -43,7 +43,7 @@
 
 // version and application name (always use this version)
 #undef VERSION
-#define VERSION                         "2.2.1cvs"
+#define VERSION                         "2.2.1"
 #define APP_NAME                        "llcon"
 
 // file name for logging file

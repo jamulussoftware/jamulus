329cf6e31f42ea9a920cf5d5246fe4591a1dde59
Volker Fischer <corrados@users.noreply.github.com>
2010-03-16 16:21:12 +0000

    prepare for next release 3.1.0

--- src/global.h
+++ src/global.h
@@ -43,7 +43,7 @@
 
 // version and application name (always use this version)
 #undef VERSION
-#define VERSION                         "3.1.0cvs"
+#define VERSION                         "3.1.0"
 #define APP_NAME                        "llcon"
 
 // file name for logging file

3b34c2561e3c7402b23e02779075ab42e6ea9cf3
Volker Fischer <corrados@users.noreply.github.com>
2010-04-11 18:04:06 +0000

    some advances in deployment of llcon software

--- mac/deploy_mac.sh
+++ mac/deploy_mac.sh
@@ -1,9 +1,24 @@
 #!/bin/sh
 cd ..
+
+
+# TODO add tag to CVS with current version number
+# cvs -n update 2>null | grep -i "M " # error if any file is locally modified!!! -> TODO
+
+
+# first clean up
 rm -rf llcon.app
 make clean
+
+# make everything
 make -j2
+
+# call qt mac deploy tool
 macdeployqt llcon.app -dmg
+
+# create zip file including COPYING file
 zip llcon-version-mac.zip llcon.dmg COPYING
+
+# cleanup and go back to original directory
 rm llcon.dmg
 cd mac
---
+++ windows/deploy_windows.bat
@@ -0,0 +1,20 @@
+@echo off
+
+rem settings and check ---------------------------------------------------------
+set NSIS_PATH=%PROGRAMFILES%\NSIS
+
+if "%VSINSTALLDIR%" == "" goto vsenvproblem
+
+rem clean and compile solution -------------------------------------------------
+devenv llcon.sln /clean "Release|Win32"
+devenv llcon.sln /build "Release|Win32"
+
+rem create installer -----------------------------------------------------------
+%NSIS_PATH%\makensis.exe installer.nsi
+
+goto endofskript
+
+:vsenvproblem
+echo Use the Visual Studio Command Prompt to call this skript
+
+:endofskript

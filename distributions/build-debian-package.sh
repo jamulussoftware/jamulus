876e767eede7d95e7c55c5ca286bfa6e6d4f983a
Your Name <you@example.com>
2019-05-28 01:56:06 +0200

    hint for builder

--- distributions/build-debian-package.sh
+++ distributions/build-debian-package.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 red="\e[91m"
 default="\e[39m"
-echo Today \(2019-05-27\) it is maybe best to build the deb it on Ubuntu 16.04
+echo Today \(2019-05-27\) it is maybe best to build the .deb it on Ubuntu 16.04
 echo Since there are no versions specified of the libraries it will takte current
 echo so it would also run on Ubuntu 17,18,19 or Debian 9/10
 echo -e ${red}press a [KEY] to continue or [CTRL]-C to abort${default}

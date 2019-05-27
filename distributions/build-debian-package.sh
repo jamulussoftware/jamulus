794901d9c9d14ae9fbe17fec86a282aae6ddee84
Your Name <you@example.com>
2019-05-28 01:55:32 +0200

    hint for builder

--- distributions/build-debian-package.sh
+++ distributions/build-debian-package.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
+red="\e[91m"
+default="\e[39m"
 echo Today \(2019-05-27\) it is maybe best to build the deb it on Ubuntu 16.04
 echo Since there are no versions specified of the libraries it will takte current
 echo so it would also run on Ubuntu 17,18,19 or Debian 9/10
-echo press a [KEY] to continue or [CTRL]-C to abort
+echo -e ${red}press a [KEY] to continue or [CTRL]-C to abort${default}
 read -n 1
 
 sudo apt-get install devscripts build-essential lintian dh-make

720feafe0c5d8363556229dd39665572617d4f74
Your Name <you@example.com>
2019-05-28 01:51:58 +0200

    hint for builder

--- distributions/build-debian-package.sh
+++ distributions/build-debian-package.sh
@@ -3,7 +3,7 @@ echo Today (2019-05-27) it is maybe best to build the deb it on Ubuntu 16.04
 echo Since there are no versions specified of the libraries it will takte current
 echo so it would also run on Ubuntu 17,18,19 or Debian 9/10
 echo press a [KEY] to continue or [CTRL]-C to abort
-read
+read -n 1
 
 sudo apt-get install devscripts build-essential lintian dh-make
 sudo apt-get install qtdeclarative5-dev qt5-default libjack-jackd2-dev

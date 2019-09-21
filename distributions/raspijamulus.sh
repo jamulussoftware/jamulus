360f5e90aa4e8a975de29d4b8e0349ec2e4dadd6
Volker Fischer <corrados@users.noreply.github.com>
2019-09-21 10:23:39 +0200

    some more work for the Raspberry Pi setup script

--- distributions/raspijamulus.sh
+++ distributions/raspijamulus.sh
@@ -1,15 +1,25 @@
 #!/bin/bash
 
 # This script is intended to setup a clean Raspberry Pi system for running Jamulus
-
-# Opus audio codec, custom compilation with custom interface and fixed point
 OPUS="opus-1.3"
-if [ ! -d "${OPUS}" ]; then
+
+echo "TODO: sudo apt-get install [needed libraries for compilation and runtime]"
+
+# Opus audio codec, custom compilation with custom modes and fixed point support
+if [ -d "${OPUS}" ]; then
+  echo "The Opus directory is present, we assume it is compiled and ready to use. If not, delete the opus directory and call this script again."
+else
   wget https://archive.mozilla.org/pub/opus/${OPUS}.tar.gz
   tar -xzf ${OPUS}.tar.gz
   rm ${OPUS}.tar.gz
   cd ${OPUS}
-
-  echo "TODO: configure Opus for custom interface and fixed point and compile and install"
+  ./configure --enable-custom-modes --enable-fixed-point
+  make
 fi
+
+# compile Jamulus with external Opus library
+cd ..
+qmake "CONFIG+=opus_shared_lib" Jamulus.pro
+make
+cd distrubutions
 

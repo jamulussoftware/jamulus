3c49eb9654420b19c50d6a2c380ffedd0e0b659e
Volker Fischer <corrados@users.noreply.github.com>
2019-10-01 16:50:47 +0200

    for more than one CPU core use higher audio quality

--- distributions/raspijamulus.sh
+++ distributions/raspijamulus.sh
@@ -85,9 +85,15 @@ echo "Using USB audio device: ${ADEVICE}"
 # write Jamulus ini file for setting the client name and buffer settings
 JAMULUSINIFILE="Jamulus.ini"
 NAME64=$(echo "Raspi $(hostname)"|cut -c -15|tr -d $'\n'|base64)
-echo -e "<client>\n  <name_base64>${NAME64}</name_base64>" > ${JAMULUSINIFILE}
-echo -e "  <autojitbuf>1</autojitbuf>\n  <jitbuf>3</jitbuf>\n  <jitbufserver>3</jitbufserver>" >> ${JAMULUSINIFILE}
-echo -e "  <audiochannels>0</audiochannels>\n  <audioquality>0</audioquality>\n</client>" >> ${JAMULUSINIFILE}
+if [ "$NCORES" -gt "1" ]; then
+  echo -e "<client>\n  <name_base64>${NAME64}</name_base64>" > ${JAMULUSINIFILE}
+  echo -e "  <autojitbuf>1</autojitbuf>\n  <jitbuf>3</jitbuf>\n  <jitbufserver>3</jitbufserver>" >> ${JAMULUSINIFILE}
+  echo -e "  <audiochannels>2</audiochannels>\n  <audioquality>1</audioquality>\n</client>" >> ${JAMULUSINIFILE}
+else
+  echo -e "<client>\n  <name_base64>${NAME64}</name_base64>" > ${JAMULUSINIFILE}
+  echo -e "  <autojitbuf>1</autojitbuf>\n  <jitbuf>3</jitbuf>\n  <jitbufserver>3</jitbufserver>" >> ${JAMULUSINIFILE}
+  echo -e "  <audiochannels>0</audiochannels>\n  <audioquality>0</audioquality>\n</client>" >> ${JAMULUSINIFILE}
+fi
 
 # taken from "Raspberry Pi and realtime, low-latency audio" homepage at wiki.linuxaudio.org
 #sudo service triggerhappy stop

89f5125b636ee5a6e676574e37860965fe9a0af4
Jonathan <4561747+gilgongo@users.noreply.github.com>
2020-07-04 17:20:11 +0100

    Add "clear" icon
    See https://github.com/corrados/jamulus/issues/228#issuecomment-653781054

--- src/serverdlg.cpp
+++ src/serverdlg.cpp
@@ -330,7 +330,7 @@ lvwClients->setMinimumHeight ( 140 );
     pbtNewRecording->setAutoDefault ( false );
     pbtRecordingDir->setAutoDefault ( false );
     edtRecordingDir->setText( pServer->GetRecordingDir() );
-    tbtClearRecordingDir->setText ( "(X)" ); // someone make me a nice icon?
+    tbtClearRecordingDir->setText ( ""<font color=""black"">&#9003;</font>"" );
 
     UpdateRecorderStatus ( QString::null );
 

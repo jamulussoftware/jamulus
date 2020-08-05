91d99fbd8113296008b2ed1b87a3fff5177c0fff
Volker Fischer <corrados@users.noreply.github.com>
2020-08-05 18:11:28 +0200

    allow more max. number of clients to be connected to the server since with the new multithreaded implementation the capacity of the server should increase

--- src/global.h
+++ src/global.h
@@ -180,7 +180,7 @@ LED bar:      lbr
 #define YELLOW_BOUND_LED_BAR             5
 
 // maximum number of connected clients at the server (must not be larger than 256)
-#define MAX_NUM_CHANNELS                 50 // max number channels for server
+#define MAX_NUM_CHANNELS                 100 // max number channels for server
 
 // actual number of used channels in the server
 // this parameter can safely be changed from 1 to MAX_NUM_CHANNELS

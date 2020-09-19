5300c0c5cac13afe7ea5253972aa6af5cc100523
Volker Fischer <corrados@users.noreply.github.com>
2020-09-19 11:41:49 +0200

    take the audio network packet size from the master client so that the slave clients do not get any audio back if they do not have the correct settings chosen

--- src/server.cpp
+++ src/server.cpp
@@ -1028,7 +1028,7 @@ static CTimingMeas JitterMeas ( 1000, "test2.dat" ); JitterMeas.Measure(); // TE
                     // the mix and coded audio data
                     vecChannels[iCurChanID].PrepAndSendPacket ( &Socket,
                                                                 vecvecbyCodedDataOut[0],
-                                                                vecChannels[iCurChanID].GetNetwFrameSize() );
+                                                                vecChannels[vecChanIDsCurConChan[0]].GetNetwFrameSize() );
                 }
                 else
                 {

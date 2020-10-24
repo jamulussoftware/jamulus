9758e2325f4ba4a19fba3de4a78a4a443a5f1c50
Olivier Humbert <trebmuh@users.noreply.github.com>
2020-10-24 14:13:24 +0200

    French translation update for 3.6.0

--- src/res/translation/translation_fr_FR.ts
+++ src/res/translation/translation_fr_FR.ts
@@ -986,7 +986,7 @@
     <message>
         <location filename="../../clientdlg.cpp" line="280"/>
         <source>N&amp;o Channel Sorting</source>
-        <translation type="unfinished"></translation>
+        <translation>Pas de tri des canaux (&amp;o)</translation>
     </message>
     <message>
         <location filename="../../clientdlg.cpp" line="283"/>
@@ -1006,7 +1006,7 @@
     <message>
         <location filename="../../clientdlg.cpp" line="292"/>
         <source>Sort Channel Users by &amp;City</source>
-        <translation type="unfinished"></translation>
+        <translation>Trier les utilisateurs du &amp;canal par ville</translation>
     </message>
     <message>
         <location filename="../../clientdlg.cpp" line="322"/>
@@ -2591,17 +2591,17 @@
     <message>
         <location filename="../../util.cpp" line="1103"/>
         <source>Mountain Dulcimer</source>
-        <translation type="unfinished"></translation>
+        <translation>Dulcimer de montagne</translation>
     </message>
     <message>
         <location filename="../../util.cpp" line="1104"/>
         <source>Scratching</source>
-        <translation type="unfinished"></translation>
+        <translation>Scratch</translation>
     </message>
     <message>
         <location filename="../../util.cpp" line="1105"/>
         <source>Rapping</source>
-        <translation type="unfinished"></translation>
+        <translation>Rap</translation>
     </message>
     <message>
         <location filename="../../settings.cpp" line="293"/>

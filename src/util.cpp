a8873e69334feb457e2d55193f576b135323c597
Volker Fischer <corrados@users.noreply.github.com>
2020-11-06 16:59:03 +0100

    merge fix

--- src/util.cpp
+++ src/util.cpp
@@ -494,6 +494,7 @@ CAboutDlg::CAboutDlg ( QWidget* parent ) : QDialog ( parent )
         "<p>Volker Fischer (<a href=""https://github.com/corrados"">corrados</a>)</p>"
         "<p><b>" + tr ( "Polish" ) + "</b></p>"
         "<p>Martyna Danysz (<a href=""https://github.com/Martyna27"">Martyna27</a>)</p>"
+        "<p>SeeLook (<a href=""https://github.com/SeeLook"">SeeLook</a>)</p>"
         "<p><b>" + tr ( "Swedish" ) + "</b></p>"
         "<p>Daniel (<a href=""https://github.com/genesisproject2020"">genesisproject2020</a>)</p>"
         "<p><b>" + tr ( "Slovak" ) + "</b></p>"

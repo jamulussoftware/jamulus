3ecf0250ddeedd9a1352fa047c0cc3e9d2de0de7
ann0see <20726856+ann0see@users.noreply.github.com>
2020-11-07 20:56:04 +0100

    Remove no register central server

--- src/main.cpp
+++ src/main.cpp
@@ -8,16 +8,16 @@
  *
  * This program is free software; you can redistribute it and/or modify it under
  * the terms of the GNU General Public License as published by the Free Software
- * Foundation; either version 2 of the License, or (at your option) any later 
+ * Foundation; either version 2 of the License, or (at your option) any later
  * version.
  *
- * This program is distributed in the hope that it will be useful, but WITHOUT 
+ * This program is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
- * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more 
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
  * details.
  *
  * You should have received a copy of the GNU General Public License along with
- * this program; if not, write to the Free Software Foundation, Inc., 
+ * this program; if not, write to the Free Software Foundation, Inc.,
  * 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA
  *
 \******************************************************************************/
@@ -75,7 +75,6 @@ int main ( int argc, char** argv )
     bool         bUseTranslation             = true;
     bool         bCustomPortNumberGiven      = false;
     bool         bEduModeEnabled             = false;
-    bool         bAllowRegisterEduMode       = false;
     int          iNumServerChannels          = DEFAULT_USED_NUM_CHANNELS;
     int          iCtrlMIDIChannel            = INVALID_MIDI_CH;
     quint16      iPortNumber                 = DEFAULT_PORT_NUMBER;
@@ -444,18 +443,6 @@ int main ( int argc, char** argv )
             continue;
         }
 
-        // Allow edu mode servers on central server ----------------------------
-        if ( GetFlagArgument ( argv,
-                               i,
-                               "--allowedumode",
-                               "--allowedumode" ) )
-        {
-            bAllowRegisterEduMode = true;
-            tsConsole << "- Edu-Mode Servers can register on this central server" << endl;
-            CommandLineOptions << "--allowedumode";
-            continue;
-        }
-
         // Server welcome message ----------------------------------------------
         if ( GetStringArgument ( tsConsole,
                                  argc,
@@ -790,7 +777,7 @@ int main ( int argc, char** argv )
             tsConsole << generr.GetErrorText() << endl;
         }
     }
-    
+
 #if defined ( __APPLE__ ) || defined ( __MACOSX )
     activity.EndActivity();
 #endif

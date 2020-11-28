5221fcdfa0e61c2de981576731286f8dd3e09b62
Volker Fischer <corrados@users.noreply.github.com>
2020-11-28 14:54:33 +0100

    undo latest change since it did not fix the issue

--- mac/sound.cpp
+++ mac/sound.cpp
@@ -992,11 +992,8 @@ OSStatus CSound::deviceNotification ( AudioDeviceID,
          ( inAddresses->mSelector == kAudioHardwarePropertyDefaultOutputDevice ) ||
          ( inAddresses->mSelector == kAudioHardwarePropertyDefaultInputDevice ) )
     {
-        // reload the driver list of available sound devices if necessary
-        if ( inAddresses->mSelector != kAudioDevicePropertyDeviceHasChanged )
-        {
-            pSound->GetAvailableInOutDevices();
-        }
+        // reload the driver list of available sound devices
+        pSound->GetAvailableInOutDevices();
 
         // if any property of the device has changed, do a full reload
         pSound->EmitReinitRequestSignal ( RS_RELOAD_RESTART_AND_INIT );

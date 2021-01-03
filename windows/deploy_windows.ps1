6a3837e3c103913db9f882b731ce0d2f83e9ecae
ann0see <20726856+ann0see@users.noreply.github.com>
2021-01-03 21:42:25 +0100

    Update deploy_windows.ps1

--- windows/deploy_windows.ps1
+++ windows/deploy_windows.ps1
@@ -10,7 +10,6 @@ param(
 )
 # for gh actions
 if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) { Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs; exit }
-Update-Module PackageManagement
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 
 Set-Location -Path "$PSScriptRoot\..\"

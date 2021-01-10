2b4ea10a651bf98a170d99875a58a6e784c721e2
ann0see <20726856+ann0see@users.noreply.github.com>
2021-01-10 22:25:40 +0100

    Another typo fixed

--- .github/actions_scripts/get_release_vars.sh
+++ .github/actions_scripts/get_release_vars.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 VERSION=$(cat ${1}/Jamulus.pro | grep -oP 'VERSION = \K\w[^\s\\]*')
-echo ":set-output name=JAMULUS_VERSION::${VERSION}"
+echo "::set-output name=JAMULUS_VERSION::${VERSION}"

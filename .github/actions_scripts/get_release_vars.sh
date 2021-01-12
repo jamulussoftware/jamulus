346cf70dedc55af0cb876c188a9f0fe2ae67df69
ann0see <20726856+ann0see@users.noreply.github.com>
2021-01-12 20:56:03 +0100

    Get ChangeLog contents via perl script

--- .github/actions_scripts/get_release_vars.sh
+++ .github/actions_scripts/get_release_vars.sh
@@ -2,3 +2,5 @@
 
 VERSION=$(cat ${1}/Jamulus.pro | grep -oP 'VERSION = \K\w[^\s\\]*')
 echo "::set-output name=JAMULUS_VERSION::${VERSION}"
+
+perl ${1}/.github/actions_scripts/getChangelog.pl ${1}/ChangeLog ${VERSION} > ${1}/autoLatestChangelog.md

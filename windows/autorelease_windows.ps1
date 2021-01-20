2a0c60c3babc1de3da651926d352bd66b473e880
ann0see <20726856+ann0see@users.noreply.github.com>
2021-01-20 20:00:07 +0100

    Fix mirror

--- windows/autorelease_windows.ps1
+++ windows/autorelease_windows.ps1
@@ -7,9 +7,9 @@ echo "Install Qt..."
 # Install Qt
 pip install aqtinstall
 echo "Get Qt 64 bit..."
-aqt install --outputdir C:\Qt 5.15.2 windows desktop win64_msvc2019_64
+aqt install --outputdir C:\Qt 5.15.2 windows desktop win64_msvc2019_64 -b https://mirrors.ocf.berkeley.edu/qt/
 echo "Get Qt 32 bit..."
-aqt install --outputdir C:\Qt 5.15.2 windows desktop win32_msvc2019
+aqt install --outputdir C:\Qt 5.15.2 windows desktop win32_msvc2019 -b https://mirrors.ocf.berkeley.edu/qt/
 
 echo "Build installer..."
 # Build the installer

6f741fb62094ee935bd31ed2b3d11e460a801831
nefarius2001 <nefarius2001-bitbucket@yahoo.de>
2021-01-22 00:29:56 +0100

    cleanup comments & console

--- .github/actions_scripts/analyse_git_reference.py
+++ .github/actions_scripts/analyse_git_reference.py
@@ -12,20 +12,21 @@ release_version_name = jamulus_version
 
 if fullref.startswith("refs/tags/"):
     if "beta" in pushed_name:
-        print('this is a Beta-Release-Tag')
+        print('this reference is a Beta-Release-Tag')
         is_prerelease = True
     else:
-        print('this is a Release-Tag')
+        print('this reference is a Release-Tag')
         is_prerelease = True
     if pushed_name == "latest":
-        print('this is a Latest-Tag')
+        print('this reference is a Latest-Tag')
         release_version_name = "latest"
         release_title='Release "latest"'
     else:
+        print('this reference is a Unknown-Type Tag')
         release_version_name = jamulus_version
         release_title="Release {}  ({})".format(release_version_name, pushed_name)
 elif fullref.startswith("refs/heads/"):
-    print('python is Head')
+    print('this reference is a Head/Branch')
     is_prerelease=True
     release_title='Pre-Release of "{}"'.format(pushed_name)
 else:
@@ -33,11 +34,13 @@ else:
     release_title='Pre-Release of "{}"'.format(pushed_name)
     is_prerelease=True
 
-print("IS_PRERELEASE::{}".format(is_prerelease)) #debug output
-print("RELEASE_TITLE::{}".format(release_title)) #debug output
-print("::set-output name=IS_PRERELEASE::{}".format(str(is_prerelease).lower()))
-print("::set-output name=RELEASE_TITLE::{}".format(release_title))
-print("::set-output name=RELEASE_TAG::{}".format(pushed_name))
-print("::set-output name=PUSHED_NAME::{}".format(pushed_name))
-print("::set-output name=JAMULUS_VERSION::{}".format(jamulus_version))
-print("::set-output name=RELEASE_VERSION_NAME::{}".format(release_version_name))
+def set_github_variable(varname, varval):
+    print("{}='{}'".format(varname, varval)) #console output
+    print("::set-output name={}::{}".format(varname, varval))
+
+set_github_variable("IS_PRERELEASE", str(is_prerelease).lower())
+set_github_variable("RELEASE_TITLE", release_title)
+set_github_variable("RELEASE_TAG", "releasetag/"+pushed_name) #better not use pure pushed name, creates a tag with the name of the branch, leads to ambiguous references => can not push to this branch easily
+set_github_variable("PUSHED_NAME", pushed_name)
+set_github_variable("JAMULUS_VERSION", jamulus_version)
+set_github_variable("RELEASE_VERSION_NAME", release_version_name)

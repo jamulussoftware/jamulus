a6fc7dca4d6314c976dffa8204e7e1504462fa5f
nefarius2001 <nefarius2001-bitbucket@yahoo.de>
2021-01-23 01:24:47 +0100

    cleaner release-tags, remove debug prints (only on error)

--- .github/actions_scripts/analyse_git_reference.py
+++ .github/actions_scripts/analyse_git_reference.py
@@ -2,8 +2,11 @@
 
 import sys
 
-print('Number of arguments:', len(sys.argv), 'arguments.')
-print('Argument List:', str(sys.argv))
+if len(sys.argv) != 4:
+    print('Expecing 4 arguments, 1 script path and 3 parameters')
+    print('Number of arguments:', len(sys.argv), 'arguments.')
+    print('Argument List:', str(sys.argv))
+    raise Exception("wrong agruments")
 
 fullref=sys.argv[1]
 pushed_name=sys.argv[2]
@@ -11,6 +14,7 @@ jamulus_version=sys.argv[3]
 release_version_name = jamulus_version
 
 if fullref.startswith("refs/tags/"):
+    release_tag = pushed_name # tag already exists
     if "beta" in pushed_name:
         print('this reference is a Beta-Release-Tag')
         is_prerelease = True
@@ -29,10 +33,12 @@ elif fullref.startswith("refs/heads/"):
     print('this reference is a Head/Branch')
     is_prerelease=True
     release_title='Pre-Release of "{}"'.format(pushed_name)
+    release_tag = "releasetag/"+pushed_name #better not use pure pushed name, creates a tag with the name of the branch, leads to ambiguous references => can not push to this branch easily
 else:
     print('unknown git-reference type')
-    release_title='Pre-Release of "{}"'.format(pushed_name)
     is_prerelease=True
+    release_title='Pre-Release of "{}"'.format(pushed_name)
+    release_tag = "releasetag/"+pushed_name #avoid ambiguity in references in all cases
 
 def set_github_variable(varname, varval):
     print("{}='{}'".format(varname, varval)) #console output
@@ -40,7 +46,7 @@ def set_github_variable(varname, varval):
 
 set_github_variable("IS_PRERELEASE", str(is_prerelease).lower())
 set_github_variable("RELEASE_TITLE", release_title)
-set_github_variable("RELEASE_TAG", "releasetag/"+pushed_name) #better not use pure pushed name, creates a tag with the name of the branch, leads to ambiguous references => can not push to this branch easily
+set_github_variable("RELEASE_TAG", "releasetag/"+pushed_name) 
 set_github_variable("PUSHED_NAME", pushed_name)
 set_github_variable("JAMULUS_VERSION", jamulus_version)
 set_github_variable("RELEASE_VERSION_NAME", release_version_name)

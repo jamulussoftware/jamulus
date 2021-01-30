72ff3ae2c199d891c59863070b510107be9e487a
Volker Fischer <corrados@users.noreply.github.com>
2021-01-30 08:50:36 +0100

    Get rid of fischvolk.de (#919)

--- src/global.h
+++ src/global.h
@@ -94,14 +94,14 @@ LED bar:      lbr
 #define DOUBLE_SYSTEM_FRAME_SIZE_SAMPLES ( 2 * SYSTEM_FRAME_SIZE_SAMPLES )
 
 // default server address and port numbers
-#define DEFAULT_SERVER_ADDRESS           "jamulus.fischvolk.de"
+#define DEFAULT_SERVER_ADDRESS           "anygenre1.jamulus.io"
 #define DEFAULT_PORT_NUMBER              22124
-#define CENTSERV_ANY_GENRE2              "jamulusanygenre2.fischvolk.de:22224"
-#define CENTSERV_ANY_GENRE3              "jamulusanygenre3.fischvolk.de:22624"
-#define CENTSERV_GENRE_ROCK              "jamulusrock.fischvolk.de:22424"
-#define CENTSERV_GENRE_JAZZ              "jamulusjazz.fischvolk.de:22324"
-#define CENTSERV_GENRE_CLASSICAL_FOLK    "jamulusclassical.fischvolk.de:22524"
-#define CENTSERV_GENRE_CHORAL            "jamuluschoral.fischvolk.de:22724"
+#define CENTSERV_ANY_GENRE2              "anygenre2.jamulus.io:22224"
+#define CENTSERV_ANY_GENRE3              "anygenre3.jamulus.io:22624"
+#define CENTSERV_GENRE_ROCK              "rock.jamulus.io:22424"
+#define CENTSERV_GENRE_JAZZ              "jazz.jamulus.io:22324"
+#define CENTSERV_GENRE_CLASSICAL_FOLK    "classical.jamulus.io:22524"
+#define CENTSERV_GENRE_CHORAL            "choral.jamulus.io:22724"
 
 // getting started and software manual URL
 #define CLIENT_GETTING_STARTED_URL       "https://jamulus.io/wiki/Getting-Started"

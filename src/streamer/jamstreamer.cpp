cc48ad5a9211a4cb68bea8532511687f1d30de79
dingodoppelt <dexxter@top-email.net>
2021-02-10 11:56:46 +0100

    refactor, get rid of redundant code (thanks to npostavs)

--- src/streamer/jamstreamer.cpp
+++ src/streamer/jamstreamer.cpp
@@ -3,30 +3,19 @@
 
 using namespace streamer;
 
-// --- CONSTRUCTOR ---
 CJamStreamer::CJamStreamer() {}
 
-// --- PROCESS ---
 // put the received pcm data into the pipe to ffmpeg
 void CJamStreamer::process( int iServerFrameSizeSamples, CVector<int16_t> data ) {
-    if (pcm == 0) // thanks to hselasky
-    {
-        pcm = new int16_t [2 * iServerFrameSizeSamples];
-    }
-    for ( int i = 0; i < ( 2 * iServerFrameSizeSamples ); i++ )
-    {
-        pcm[i] = data[i];
-    }
-    fwrite(pcm, 2, ( 2 * iServerFrameSizeSamples ), pipeout);
+    fwrite(&data[0], 2, ( 2 * iServerFrameSizeSamples ), pipeout);
 }
 
 void CJamStreamer::Init( const QString strStreamDest ) {
     this->strStreamDest = strStreamDest;
 }
 
+// create a pipe to ffmpeg called "pipeout" to being able to put out the pcm data to it
 void CJamStreamer::OnStarted() {
-    // create a pipe to ffmpeg called "pipeout" to being able to put out the pcm data to it
-    pcm = 0; // it is necessary to initialise pcm
     QString command = "ffmpeg -y -f s16le -ar 48000 -ac 2 -i - " + strStreamDest;
     pipeout = popen(command.toUtf8().constData(), "w");
 }
--- src/streamer/jamstreamer.h
+++ src/streamer/jamstreamer.h
@@ -19,7 +19,6 @@ public slots:
 private:
     QString strStreamDest; // stream destination to pass to ffmpeg as output part of arguments
     FILE *pipeout; // pipe for putting out the pcm data to ffmpeg
-    int16_t *pcm; // pointer to pcm which will hold the raw pcm data from the server, to be initialised in the constructor
 };
 }
 #endif

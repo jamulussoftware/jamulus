27c1f4ca61eecffcf123805d1f6f6f671d2e541a
nefarius2001 <nefarius2001-bitbucket@yahoo.de>
2021-02-13 22:56:03 +0100

    small fix

--- linux/autobuild/artifacts/autobuild_deb_3_copy_files.sh
+++ linux/autobuild/artifacts/autobuild_deb_3_copy_files.sh
@@ -41,7 +41,7 @@ echo ""
 
 #debuild -b -us -uc -aarmhf
 # copy for auto release
-#cp ${1}/../*.deb ${1}/deploy/
+#cp ${THIS_JAMULUS_PROJECT_PATH}/../*.deb ${THIS_JAMULUS_PROJECT_PATH}/deploy/
 
 echo ""
 echo ""
@@ -56,7 +56,7 @@ echo ""
 echo ""
 artifact_deploy_filename_1=jamulus_headless_${jamulus_buildversionstring}_ubuntu_amd64.deb
 echo "Move/Rename the built file to deploy/${artifact_deploy_filename_1}"
-mv ${1}/../jamulus-headless*_amd64.deb ${THIS_JAMULUS_PROJECT_PATH}/deploy/${artifact_deploy_filename_1}
+mv ${THIS_JAMULUS_PROJECT_PATH}/../jamulus-headless*_amd64.deb ${THIS_JAMULUS_PROJECT_PATH}/deploy/${artifact_deploy_filename_1}
 
 #move/rename normal  second
 echo ""

45e217c66007c9d36bc3f52921c2fb4df3815b54
ngocdh <huyngocbk@gmail.com>
2021-04-15 14:09:34 +0200

    Update sound.h

--- android/sound.h
+++ android/sound.h
@@ -71,17 +71,42 @@ public:
     };
 
 /*NGOCDH */
+    DECLARE_JNI_CLASS (AndroidContext, "android/content/Context")
+    LocalRef<jobject> getAppContext() noexcept
+    {
+        auto* env = getEnv();
+        auto context = androidApkContext;
+
+        // You did not call Thread::initialiseJUCE which must be called at least once in your apk
+        // before using any JUCE APIs. The Projucer will automatically generate java code
+        // which will invoke Thread::initialiseJUCE for you.
+        jassert (env != nullptr && context != nullptr);
+
+        if (context == nullptr)
+            return LocalRef<jobject>();
+
+        if (env->IsInstanceOf (context, AndroidApplication) != 0)
+            return LocalRef<jobject> (env->NewLocalRef (context));
+
+        LocalRef<jobject> applicationContext (env->CallObjectMethod (context, AndroidContext.getApplicationContext));
+
+        if (applicationContext == nullptr)
+            return LocalRef<jobject> (env->NewLocalRef (context));
+
+        return applicationContext;
+    }
+
+
+
     QString getAvailableDevices()
     {
-        if (! supportsDevicesInfo())
-            return;
 
         auto* env = getEnv();
 
         jclass audioManagerClass = env->FindClass ("android/media/AudioManager");
 
         // We should be really entering here only if API supports it.
-        jassert (audioManagerClass != nullptr);
+        //jassert (audioManagerClass != nullptr);
 
         if (audioManagerClass == nullptr)
             return;
@@ -100,100 +125,19 @@ public:
 
         const int numDevices = env->GetArrayLength (devices.get());
 
+
+        QString strAvailableDevices = "";
         for (int i = 0; i < numDevices; ++i)
         {
             auto device = LocalRef<jobject> ((jobject) env->GetObjectArrayElement (devices.get(), i));
-            addDevice (device, env);
+            strAvailableDevices = strAvailableDevices + getDeviceInfo (device, env);
         }
 
-        QString strAvailableDevices = "Inputs: ";
-        //JUCE_OBOE_LOG ("-----InputDevices:");
 
-        for (auto& device : inputDevices)
-        {
-            strAvailableDevices = strAvailableDevices + "name = " + device.name;
-            strAvailableDevices = strAvailableDevices + ", id = " + String (device.id) + "; ";
-        }
-
-        //JUCE_OBOE_LOG ("-----OutputDevices:");
-        strAvailableDevices = strAvailableDevices + " | Outputs: ";
-        for (auto& device : outputDevices)
-        {
-            strAvailableDevices = strAvailableDevices + "name = " + device.name;
-            strAvailableDevices = strAvailableDevices + ", id = " + QString (device.id) + "; ";
-        }
+        
+        return strAvailableDevices;
     }
     
-    bool supportsDevicesInfo() const
-    {
-        static auto result = getAndroidSDKVersion() >= 23;
-        return result;
-    }
-
-    void addDevice (const LocalRef<jobject>& device, JNIEnv* env)
-    {
-        auto deviceClass = LocalRef<jclass> ((jclass) env->FindClass ("android/media/AudioDeviceInfo"));
-
-        jmethodID getProductNameMethod = env->GetMethodID (deviceClass, "getProductName",
-                                                           "()Ljava/lang/CharSequence;");
-
-        jmethodID getTypeMethod          = env->GetMethodID (deviceClass, "getType", "()I");
-        jmethodID getIdMethod            = env->GetMethodID (deviceClass, "getId", "()I");
-        jmethodID getSampleRatesMethod   = env->GetMethodID (deviceClass, "getSampleRates", "()[I");
-        jmethodID getChannelCountsMethod = env->GetMethodID (deviceClass, "getChannelCounts", "()[I");
-        jmethodID isSourceMethod         = env->GetMethodID (deviceClass, "isSource", "()Z");
-
-        auto deviceTypeString = deviceTypeToString (env->CallIntMethod (device, getTypeMethod));
-
-        if (deviceTypeString.isEmpty()) // unknown device
-            return;
-
-        auto name = QString ((jstring) env->CallObjectMethod (device, getProductNameMethod)) + " " + deviceTypeString;
-        auto id = env->CallIntMethod (device, getIdMethod);
-        auto isInput  = env->CallBooleanMethod (device, isSourceMethod);
-        auto& devices = isInput ? inputDevices : outputDevices;
-
-        devices.add ({ name, id, [], [] });
-    }
-
-    static QString deviceTypeToString (int type)
-    {
-        switch (type)
-        {
-            case 0:   return {};
-            case 1:   return "built-in earphone speaker";
-            case 2:   return "built-in speaker";
-            case 3:   return "wired headset";
-            case 4:   return "wired headphones";
-            case 5:   return "line analog";
-            case 6:   return "line digital";
-            case 7:   return "Bluetooth device typically used for telephony";
-            case 8:   return "Bluetooth device supporting the A2DP profile";
-            case 9:   return "HDMI";
-            case 10:  return "HDMI audio return channel";
-            case 11:  return "USB device";
-            case 12:  return "USB accessory";
-            case 13:  return "DOCK";
-            case 14:  return "FM";
-            case 15:  return "built-in microphone";
-            case 16:  return "FM tuner";
-            case 17:  return "TV tuner";
-            case 18:  return "telephony";
-            case 19:  return "auxiliary line-level connectors";
-            case 20:  return "IP";
-            case 21:  return "BUS";
-            case 22:  return "USB headset";
-            case 23:  return "hearing aid";
-            case 24:  return "built-in speaker safe";
-            case 25:  return {};
-            default:  jassertfalse; return {}; // type not supported yet, needs to be added!
-        }
-    }
-    
-    inline LocalRef<jstring> javaString (const QString& s)
-    {
-        return LocalRef<jstring> (getEnv()->NewStringUTF (s.toUtf8()));//(s.toUTF8()));
-    }
 /*end of NGOCDH */    
 
 protected:
@@ -241,7 +185,7 @@ private:
         // You did not call Thread::initialiseJUCE which must be called at least once in your apk
         // before using any JUCE APIs. The Projucer will automatically generate java code
         // which will invoke Thread::initialiseJUCE for you.
-        jassertfalse;
+        //jassertfalse;
         return nullptr;
     }
 
@@ -255,12 +199,12 @@ private:
     JNIEXPORT jint JNI_OnLoad(JavaVM* vm, void* /*reserved*/)
     {
         // Huh? JNI_OnLoad was called two times!
-        jassert (androidJNIJavaVM == nullptr);
+        //jassert (androidJNIJavaVM == nullptr);
 
         androidJNIJavaVM = vm;
 
         auto* env = getEnv();
-
+/*
         // register the initialisation function
         auto juceJavaClass = env->FindClass("com/rmsl/juce/Java");
 
@@ -270,7 +214,7 @@ private:
                                     reinterpret_cast<void*> (javainitialiseJamulus)};
 
             auto status = env->RegisterNatives (juceJavaClass, &method, 1);
-            jassert (status == 0);
+            //jassert (status == 0);
         }
         else
         {
@@ -279,7 +223,7 @@ private:
             // call Thread::initialiseJUCE manually
             env->ExceptionClear();
         }
-
+*/
         JNIClassBase::initialiseAllClasses (env);
 
         return JNI_VERSION_1_6;
@@ -291,7 +235,7 @@ private:
         ScopedLock lock (cs);
 
         // jniEnv and context should not be null!
-        jassert (jniEnv != nullptr && context != nullptr);
+        //jassert (jniEnv != nullptr && context != nullptr);
 
         auto* env = static_cast<JNIEnv*> (jniEnv);
 
@@ -300,11 +244,124 @@ private:
             JavaVM* javaVM = nullptr;
 
             auto status = env->GetJavaVM (&javaVM);
-            jassert (status == 0 && javaVM != nullptr);
+            //jassert (status == 0 && javaVM != nullptr);
 
             androidJNIJavaVM = javaVM;
         }
 
     }
     
+    
+
+    QString getDeviceInfo (const LocalRef<jobject>& device, JNIEnv* env)
+    {
+        auto deviceClass = LocalRef<jclass> ((jclass) env->FindClass ("android/media/AudioDeviceInfo"));
+
+        jmethodID getProductNameMethod = env->GetMethodID (deviceClass, "getProductName",
+                                                           "()Ljava/lang/CharSequence;");
+
+        jmethodID getTypeMethod          = env->GetMethodID (deviceClass, "getType", "()I");
+        jmethodID getIdMethod            = env->GetMethodID (deviceClass, "getId", "()I");
+        jmethodID getSampleRatesMethod   = env->GetMethodID (deviceClass, "getSampleRates", "()[I");
+        jmethodID getChannelCountsMethod = env->GetMethodID (deviceClass, "getChannelCounts", "()[I");
+        jmethodID isSourceMethod         = env->GetMethodID (deviceClass, "isSource", "()Z");
+
+        auto deviceTypeString = deviceTypeToString (env->CallIntMethod (device, getTypeMethod));
+
+        if (deviceTypeString.isEmpty()) // unknown device
+            return;
+
+        auto name = QString ((jstring) env->CallObjectMethod (device, getProductNameMethod)) + " " + deviceTypeString;
+        auto id = env->CallIntMethod (device, getIdMethod);
+        auto isInput  = env->CallBooleanMethod (device, isSourceMethod);
+        auto& devices = isInput ? inputDevices : outputDevices;
+
+        return (isInput ? QString("In: "):QString("Out: ")) + name + " - " + id + "; ";
+    }
+
+    static QString deviceTypeToString (int type)
+    {
+        switch (type)
+        {
+            case 0:   return {};
+            case 1:   return "built-in earphone speaker";
+            case 2:   return "built-in speaker";
+            case 3:   return "wired headset";
+            case 4:   return "wired headphones";
+            case 5:   return "line analog";
+            case 6:   return "line digital";
+            case 7:   return "Bluetooth device typically used for telephony";
+            case 8:   return "Bluetooth device supporting the A2DP profile";
+            case 9:   return "HDMI";
+            case 10:  return "HDMI audio return channel";
+            case 11:  return "USB device";
+            case 12:  return "USB accessory";
+            case 13:  return "DOCK";
+            case 14:  return "FM";
+            case 15:  return "built-in microphone";
+            case 16:  return "FM tuner";
+            case 17:  return "TV tuner";
+            case 18:  return "telephony";
+            case 19:  return "auxiliary line-level connectors";
+            case 20:  return "IP";
+            case 21:  return "BUS";
+            case 22:  return "USB headset";
+            case 23:  return "hearing aid";
+            case 24:  return "built-in speaker safe";
+            case 25:  return {};
+            default:  return {};//jassertfalse; return {}; // type not supported yet, needs to be added!
+        }
+    }
+    
+    inline LocalRef<jstring> javaString (const QString& s)
+    {
+        return LocalRef<jstring> (getEnv()->NewStringUTF (s.toUtf8()));//(s.toUTF8()));
+    }
+    
+};
+
+template <typename JavaType>
+class LocalRef
+{
+public:
+    explicit inline LocalRef() noexcept                 : obj (nullptr) {}
+    explicit inline LocalRef (JavaType o) noexcept      : obj (o) {}
+    inline LocalRef (const LocalRef& other) noexcept    : obj (retain (other.obj)) {}
+    inline LocalRef (LocalRef&& other) noexcept         : obj (nullptr) { std::swap (obj, other.obj); }
+    ~LocalRef()                                         { clear(); }
+
+    void clear()
+    {
+        if (obj != nullptr)
+        {
+            getEnv()->DeleteLocalRef (obj);
+            obj = nullptr;
+        }
+    }
+
+    LocalRef& operator= (const LocalRef& other)
+    {
+        JavaType newObj = retain (other.obj);
+        clear();
+        obj = newObj;
+        return *this;
+    }
+
+    LocalRef& operator= (LocalRef&& other)
+    {
+        clear();
+        std::swap (other.obj, obj);
+        return *this;
+    }
+
+    inline operator JavaType() const noexcept   { return obj; }
+    inline JavaType get() const noexcept        { return obj; }
+
+private:
+    JavaType obj;
+
+    static JavaType retain (JavaType obj)
+    {
+        return obj == nullptr ? nullptr : (JavaType) getEnv()->NewLocalRef (obj);
+    }
 };

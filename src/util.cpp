99cb2ea7691b727b1e7cf1b54e1886d8c8d1aa5b
jujudusud <jujudusud@gmail.com>
2021-05-04 00:03:14 +0200

    adding jujudusud as contributor

--- src/util.cpp
+++ src/util.cpp
@@ -491,6 +491,7 @@ CAboutDlg::CAboutDlg ( QWidget* parent ) : CBaseDlg ( parent )
         "<p>Ferenc Wágner (<a href=\"https://github.com/wferi\">wferi</a>)</p>"
         "<p>Martin Kaistra (<a href=\"https://github.com/djfun\">djfun</a>)</p>"
         "<p>Burkhard Volkemer (<a href=\"https://github.com/buv\">buv</a>)</p>"
+        "<p>Julien Taverna (<a href=\"https://github.com/jujudusud\">jujudusud</a>)</p>"
         "<br>" + tr ( "For details on the contributions check out the " ) +
         "<a href=\"https://github.com/jamulussoftware/jamulus/graphs/contributors\">" + tr ( "Github Contributors list" ) + "</a>." );
 

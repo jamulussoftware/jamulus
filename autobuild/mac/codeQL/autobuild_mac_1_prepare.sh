18be717e873294f7d159ce6128e8819f8905d7ff
Tony Mountifield <tony@mountifield.org>
2021-05-27 21:37:25 +0100

    Require Qt version to be passed as script argument

--- autobuild/mac/codeQL/autobuild_mac_1_prepare.sh
+++ autobuild/mac/codeQL/autobuild_mac_1_prepare.sh
@@ -2,16 +2,23 @@
 
 # autobuild_1_prepare: set up environment, install Qt & dependencies
 
+if [ "$#" -ne "1" ]; then
+    echo "need to specify Qt version"
+    exit 1
+fi
+
+QT_VER=$1
+
 ###################
 ###  PROCEDURE  ###
 ###################
 
 echo "Install dependencies..."
 python3 -m pip install aqtinstall
-python3 -m aqt install --outputdir /usr/local/opt/qt 5.12.10 mac desktop
+python3 -m aqt install --outputdir /usr/local/opt/qt ${QT_VER} mac desktop
 
 # add the qt binaries to the path
-export -p PATH=/usr/local/opt/qt/5.12.10/clang_64/bin:"${PATH}"
+export -p PATH=/usr/local/opt/qt/${QT_VER}/clang_64/bin:"${PATH}"
 echo "::set-env name=PATH::${PATH}"
 echo "the path is ${PATH}"
 

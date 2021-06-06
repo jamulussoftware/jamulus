7f407364cf6b845fe8ba167ef3f1c71e8ea102aa
DonC <64357360+dcorson-ticino-com@users.noreply.github.com>
2021-06-06 19:55:33 +0200

    Update autobuild_windowsinstaller_1_prepare.ps1

--- autobuild/windows/autobuild_windowsinstaller_1_prepare.ps1
+++ autobuild/windows/autobuild_windowsinstaller_1_prepare.ps1
@@ -12,8 +12,8 @@ echo "Install Qt..."
 pip install aqtinstall
 echo "Get Qt 64 bit..."
 # intermediate solution if the main server is down: append e.g. " -b https://mirrors.ocf.berkeley.edu/qt/" to the "aqt"-line below
-aqt install --outputdir C:\Qt 5.15.2 windows desktop win64_msvc2019_64
+aqt install --outputdir C:\Qt 6.1.0 windows desktop win64_msvc2019_64
 
 echo "Get Qt 32 bit..."
 # intermediate solution if the main server is down: append e.g. " -b https://mirrors.ocf.berkeley.edu/qt/" to the "aqt"-line below
-aqt install --outputdir C:\Qt 5.15.2 windows desktop win32_msvc2019
+aqt install --outputdir C:\Qt 6.1.0 windows desktop win32_msvc2019

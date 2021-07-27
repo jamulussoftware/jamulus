6e6e0bbc9ec45560d212618466d8a9177fde516e
Emlyn Bolton <emlyn.bolton@mac.com>
2021-07-27 15:38:42 -0700

    Add signing option to build if github secrets exist

--- autobuild/mac/artifacts/autobuild_mac_2_build.sh
+++ autobuild/mac/artifacts/autobuild_mac_2_build.sh
@@ -16,4 +16,24 @@ source "$(dirname "${BASH_SOURCE[0]}")/../../ensure_THIS_JAMULUS_PROJECT_PATH.sh
 cd "${THIS_JAMULUS_PROJECT_PATH}"
 
 echo "Run deploy script..."
-sh "${THIS_JAMULUS_PROJECT_PATH}"/mac/deploy_mac.sh
+
+# If we have certificate details, then prepare the signing
+if [ -z "${MACOS_CERTIFICATE_PWD}" ]; then
+    sh "${THIS_JAMULUS_PROJECT_PATH}"/mac/deploy_mac.sh
+else
+    echo "Setting up signing, as credentials found"
+
+    # Get the cert to a file
+    echo $MACOS_CERTIFICATE | base64 --decode > certificate.p12
+
+    # Set up a keychain for the build
+    security create-keychain -p T3mpP4ssword build.keychain
+    security default-keychain -s build.keychain
+    security unlock-keychain -p T3mpP4ssword build.keychain
+    security import certificate.p12 -k build.keychain -P $MACOS_CERTIFICATE_PWD -T /usr/bin/codesign
+    security set-key-partition-list -S apple-tool:,apple:,codesign: -s -k T3mpP4ssword build.keychain
+
+    sh "${THIS_JAMULUS_PROJECT_PATH}"/mac/deploy_mac.sh -s $MACOS_CERTIFICATE_ID
+fi
+
+

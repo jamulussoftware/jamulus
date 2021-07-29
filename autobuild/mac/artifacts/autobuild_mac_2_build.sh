feb5a29fe5b39376086bdb3533e4d70b0a434329
Emlyn Bolton <emlyn.bolton@mac.com>
2021-07-29 10:49:41 -0700

    Add notarization and staple steps

--- autobuild/mac/artifacts/autobuild_mac_2_build.sh
+++ autobuild/mac/artifacts/autobuild_mac_2_build.sh
@@ -43,6 +43,8 @@ else
     security set-key-partition-list -S apple-tool:,apple:,codesign: -s -k T3mpP4ssword build.keychain
 
     sh "${THIS_JAMULUS_PROJECT_PATH}"/mac/deploy_mac.sh -s "$MACOS_CERTIFICATE_ID"
+    # Set up the notarization and staple parts
+    echo "::set-output name=macos_signed::true"
 fi
 
 

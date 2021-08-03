df0da729781b60ad941e0e819828f30960ece0eb
Emlyn Bolton <emlyn.bolton@mac.com>
2021-08-03 08:57:15 -0700

    Put temporary keychain password also into an env var and GITHUB secret.

--- autobuild/mac/artifacts/autobuild_mac_2_build.sh
+++ autobuild/mac/artifacts/autobuild_mac_2_build.sh
@@ -27,6 +27,7 @@ if [[ -z "${MACOS_CERTIFICATE_PWD}" ||
      -z "${MACOS_CERTIFICATE}" ||
      -z "${MACOS_CERTIFICATE_ID}" ||
      -z "${NOTARIZATION_PASSWORD}" ||
+     -z "${KEYCHAIN_PASSWORD}" || 
      "${SIGN}" != "sign_if_possible" ]]; then
     sh "${THIS_JAMULUS_PROJECT_PATH}"/mac/deploy_mac.sh
 else
@@ -36,11 +37,11 @@ else
     echo ${MACOS_CERTIFICATE} | base64 --decode > certificate.p12
 
     # Set up a keychain for the build
-    security create-keychain -p T3mpP4ssword build.keychain
+    security create-keychain -p "${KEYCHAIN_PASSWORD}" build.keychain
     security default-keychain -s build.keychain
-    security unlock-keychain -p T3mpP4ssword build.keychain
+    security unlock-keychain -p "${KEYCHAIN_PASSWORD}" build.keychain
     security import certificate.p12 -k build.keychain -P "${MACOS_CERTIFICATE_PWD}" -T /usr/bin/codesign
-    security set-key-partition-list -S apple-tool:,apple:,codesign: -s -k T3mpP4ssword build.keychain
+    security set-key-partition-list -S apple-tool:,apple:,codesign: -s -k "${KEYCHAIN_PASSWORD}" build.keychain
 
     sh "${THIS_JAMULUS_PROJECT_PATH}"/mac/deploy_mac.sh -s "${MACOS_CERTIFICATE_ID}"
     # Set up the notarization and staple parts

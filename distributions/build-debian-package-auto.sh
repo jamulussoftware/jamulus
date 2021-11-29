15d29445dd28d870dbf4df02adc33b69d363dcae
Tony Mountifield <tony@mountifield.org>
2021-11-29 14:48:03 -0500

    Set dch maintainer name to GitHubActions

--- distributions/build-debian-package-auto.sh
+++ distributions/build-debian-package-auto.sh
@@ -8,9 +8,12 @@ cd ..
 # get the jamulus version from pro file
 VERSION=$(cat Jamulus.pro | grep -oP 'VERSION = \K\w[^\s\\]*')
 
+export DEBFULLNAME=GitHubActions DEBEMAIL=noemail@example.com
+
 # Generate Changelog
 echo -n generating changelog
-dch --newversion "${VERSION}" ''
+rm -f debian/changelog
+dch --create --package jamulus --empty --newversion "${VERSION}" ''
 perl .github/actions_scripts/getChangelog.pl ChangeLog "${VERSION}" | while read entry
 do
     echo -n .

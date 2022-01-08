86807c1ab88ee10ab453371b45d3a4e8a45fd606
Noam Postavsky <npostavs@users.sourceforge.net>
2022-01-08 16:12:26 -0500

    Use dch instead of hack to generate changelog (#2138)
    * Use dch instead of hack to generate changelog
    
    * Set dch maintainer name to GitHubActions
    
    * Only remove dashes from ChangeLog when given specific option
    
    * Print error if we fail to find specified version's ChangeLog section
    
    Also exit early once we're done processing the version relevant lines.
    
    Co-authored-by: Tony Mountifield <tony@mountifield.org>

--- distributions/build-debian-package-auto.sh
+++ distributions/build-debian-package-auto.sh
@@ -2,30 +2,24 @@
 
 # Create deb files
 
-# set armhf
-#sudo dpkg --add-architecture armhf
-
 cp -r debian ..
 cd ..
 
 # get the jamulus version from pro file
 VERSION=$(cat Jamulus.pro | grep -oP 'VERSION = \K\w[^\s\\]*')
 
-# patch changelog (with hack)
+export DEBFULLNAME=GitHubActions DEBEMAIL=noemail@example.com
 
-DATE=$(date "+%a, %d %b %Y %T" )
-echo "jamulus (${VERSION}-0) UNRELEASED; urgency=medium" > debian/changelog
-echo "" >> debian/changelog
-perl .github/actions_scripts/getChangelog.pl ChangeLog ${VERSION} >> debian/changelog
-echo "" >> debian/changelog
-echo " -- GitHubActions <noemail@example.com> ${DATE} +0001" >> debian/changelog
-echo "" >> debian/changelog
-cat distributions/debian/changelog >> debian/changelog
-
-# patch the control file
-# cp the modified control file here
-
-cp distributions/autobuilddeb/control debian/control
+# Generate Changelog
+echo -n generating changelog
+rm -f debian/changelog
+dch --create --package jamulus --empty --newversion "${VERSION}" ''
+perl .github/actions_scripts/getChangelog.pl ChangeLog "${VERSION}" --line-per-entry | while read entry
+do
+    echo -n .
+    dch "$entry"
+done
+echo
 
 echo "${VERSION} building..."
 

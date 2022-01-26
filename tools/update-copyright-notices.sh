0aaccfc2d8fe7c5ee03fc97668150ea6f9093de5
Christian Hoffmann <mail@hoffmann-christian.info>
2022-01-26 20:46:26 +0100

    Tools: update-copyright-notices: Fix quoting
    Codacy is only happy if variables are properly quoted.

--- tools/update-copyright-notices.sh
+++ tools/update-copyright-notices.sh
@@ -3,9 +3,9 @@ set -eu
 
 YEAR=$(date +%Y)
 echo "Updating global copyright strings..."
-sed -re 's/(Copyright.*2[0-9]{3}-)[0-9]{4}/\1'$YEAR'/g' -i src/res/translation/*.ts src/util.cpp src/aboutdlgbase.ui
+sed -re 's/(Copyright.*2[0-9]{3}-)[0-9]{4}/\1'"${YEAR}"'/g' -i src/res/translation/*.ts src/util.cpp src/aboutdlgbase.ui
 
 echo "Updating copyright comment headers..."
 for file in $(find android ios linux mac src windows -regex '.*\.\(cpp\|h\|mm\)' -not -regex '\./\(\.git\|libs/\|moc_\|ui_\).*'); do
-		sed -re 's/(\*.*Copyright.*[^-][0-9]{4})(\s*-\s*\b[0-9]{4})?\s*$/\1-'${YEAR}'/' -i "${file}"
+		sed -re 's/(\*.*Copyright.*[^-][0-9]{4})(\s*-\s*\b[0-9]{4})?\s*$/\1-'"${YEAR}"'/' -i "${file}"
 done

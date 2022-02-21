jamulussoftware/jamulus

commit 3a630330e113dc77c736762c08fb53cad0904ba8

Build scripts: Use file name for grep directly
Code Style: Do not use cat | grep due to performance reasons: 
https://bencane.com/2013/08/19/grepping-a-file-without-using-cat-and-grep-other-tricks/
Found by Codacy

mac/deploy_mac.sh
@@ -12,21 +12,21 @@ cert_name=""
 
 while getopts 'hs:' flag; do
     case "${flag}" in
-    s) 
-    cert_name=$OPTARG 
+    s)
+    cert_name=$OPTARG
     if [[ -z "$cert_name" ]]; then
         echo "Please add the name of the certificate to use: -s \"<name>\""
     fi
     # shift 2
     ;;
-    h) 
-    echo "Usage: -s <cert name> for signing mac build" 
-    exit 0 
+    h)
+    echo "Usage: -s <cert name> for signing mac build"
+    exit 0
     ;;
     *)
     exit 1
     ;;
-   
+
     esac
 done
 
@@ -44,7 +44,7 @@ build_app()
 {
     # Build Jamulus
     qmake "${project_path}" -o "${build_path}/Makefile" "CONFIG+=release" ${@:2}
-    local target_name="$(cat "${build_path}/Makefile" | sed -nE 's/^QMAKE_TARGET *= *(.*)$/\1/p')"
+    local target_name=$(sed -nE 's/^QMAKE_TARGET *= *(.*)$/\1/p' "${build_path}/Makefile")
     local job_count="$(sysctl -n hw.ncpu)"
 
     make -f "${build_path}/Makefile" -C "${build_path}" -j "${job_count}"
@@ -73,7 +73,7 @@ build_installer_image()
     local dmgbuild_bin="$(python -c 'import site; print(site.USER_BASE)')/bin/dmgbuild"
 
     # Get Jamulus version
-    local app_version="$(cat "${project_path}" | sed -nE 's/^VERSION *= *(.*)$/\1/p')"
+    local app_version="$(grep -oP 'VERSION = \K\w[^\s\\]*' Jamulus.pro)"
 
     # Build installer image
     "${dmgbuild_bin}" -s "${macdeploy_path}/deployment_settings.py" -D background="${resources_path}/installerbackground.png" \

distributions/build-debian-package-auto.sh
@@ -6,7 +6,7 @@ cp -r debian ..
 cd ..
 
 # get the jamulus version from pro file
-VERSION=$(cat Jamulus.pro | grep -oP 'VERSION = \K\w[^\s\\]*')
+VERSION=$(grep -oP 'VERSION = \K\w[^\s\\]*' Jamulus.pro)
 
 export DEBFULLNAME=GitHubActions DEBEMAIL=noemail@example.com
 

commit 2e06d1d90e01ae56a9bf31aba199a4ab223b07b0

Build: Fix Mac app version
The build logic used `grep -oP` which does not seem to be supported by
macOS' `grep`. Therefore, keep using `sed` instead.

Broken in: 3a630330e113dc77c736762c08fb53cad0904ba8

Co-authored-by: Tony Mountifield <tony@mountifield.org>

mac/deploy_mac.sh
@@ -73,7 +73,7 @@ build_installer_image()
     brew extract --version="${create_dmg_version}" create-dmg homebrew/cask
     brew install /usr/local/Homebrew/Library/Taps/homebrew/homebrew-cask/Formula/create-dmg@"${create_dmg_version}".rb
     # Get Jamulus version
-    local app_version="$(grep -oP 'VERSION = \K\w[^\s\\]*' Jamulus.pro)"
+    local app_version="$(sed -nE 's/^VERSION *= *(.*)$/\1/p' "${project_path}")"
 
     # Build installer image
 
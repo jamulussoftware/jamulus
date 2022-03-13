02a557e9b7b68c420422784bcb58363735e717ee
Christian Hoffmann <mail@hoffmann-christian.info>
2022-03-13 22:53:28 +0100

    Autobuild: Combine and simplify Mac build scripts
    Related: #2503
    
    - Move all autobuild/mac/* scripts to a single
      .github/autobuild/mac.sh script which is called for the different stages
      (setup/build/get-artifacts).
    
    - Condense redundant parameter parsing into a single step
    
    - Create functions with proper names for larger steps
    
    - Inline Github artifact output definition as it's shorter that way
    
    - Make shellcheck-clean

---
+++ .github/autobuild/mac.sh
@@ -0,0 +1,87 @@
+#!/bin/bash
+set -eu
+
+QT_DIR=/usr/local/opt/qt
+AQTINSTALL_VERSION=2.0.6
+
+if [[ ! ${QT_VERSION:-} =~ [0-9]+\.[0-9]+\..* ]]; then
+    echo "Environment variable QT_VERSION must be set to a valid Qt version"
+    exit 1
+fi
+if [[ ! ${jamulus_buildversionstring:-} =~ [0-9]+\.[0-9]+\.[0-9]+ ]]; then
+    echo "Environment variable jamulus_buildversionstring has to be set to a valid version string"
+    exit 1
+fi
+
+setup() {
+    if [[ -d "${QT_DIR}" ]]; then
+        echo "Using Qt installation from previous run (actions/cache)"
+    else
+        echo "Install dependencies..."
+        python3 -m pip install "aqtinstall==${AQTINSTALL_VERSION}"
+        python3 -m aqt install-qt --outputdir "${QT_DIR}" mac desktop "${QT_VERSION}" --archives qtbase qttools qttranslations qtmacextras
+    fi
+
+    # Add the qt binaries to the PATH.
+    # The clang_64 entry can be dropped when Qt <6.2 compatibility is no longer needed.
+    export PATH="${QT_DIR}/${QT_VERSION}/macos/bin:${QT_DIR}/${QT_VERSION}/clang_64/bin:${PATH}"
+    echo "::set-env name=PATH::${PATH}"
+}
+
+prepare_signing() {
+    [[ "${SIGN_IF_POSSIBLE:-0}" == "1" ]] || return 1
+
+    # Signing was requested, now check all prerequisites:
+    [[ -n "${MACOS_CERTIFICATE:-}" ]] || return 1
+    [[ -n "${MACOS_CERTIFICATE_ID:-}" ]] || return 1
+    [[ -n "${MACOS_CERTIFICATE_PWD:-}" ]] || return 1
+    [[ -n "${NOTARIZATION_PASSWORD:-}" ]] || return 1
+    [[ -n "${KEYCHAIN_PASSWORD:-}" ]] || return 1
+
+    echo "Signing was requested and all dependencies are satisfied"
+
+    # Put the cert to a file
+    echo "${MACOS_CERTIFICATE}" | base64 --decode > certificate.p12
+
+    # Set up a keychain for the build:
+    security create-keychain -p "${KEYCHAIN_PASSWORD}" build.keychain
+    security default-keychain -s build.keychain
+    security unlock-keychain -p "${KEYCHAIN_PASSWORD}" build.keychain
+    security import certificate.p12 -k build.keychain -P "${MACOS_CERTIFICATE_PWD}" -T /usr/bin/codesign
+    security set-key-partition-list -S apple-tool:,apple:,codesign: -s -k "${KEYCHAIN_PASSWORD}" build.keychain
+
+    # Tell Github Workflow that we need notarization & stapling:
+    echo "::set-output name=macos_signed::true"
+    return 0
+}
+
+build_app_as_dmg_installer() {
+    # Mac's bash version considers BUILD_ARGS unset without at least one entry:
+    BUILD_ARGS=("")
+    if prepare_signing; then
+        BUILD_ARGS=("-s" "${MACOS_CERTIFICATE_ID}")
+    fi
+    ./mac/deploy_mac.sh "${BUILD_ARGS[@]}"
+}
+
+pass_artifact_to_job() {
+    artifact_deploy_filename="jamulus_${jamulus_buildversionstring}_mac${ARTIFACT_SUFFIX:-}.dmg"
+    echo "Moving build artifact to deploy/${artifact_deploy_filename}"
+    mv ./deploy/Jamulus-*installer-mac.dmg "./deploy/${artifact_deploy_filename}"
+    echo "::set-output name=artifact_1::${artifact_deploy_filename}"
+}
+
+case "${1:-}" in
+    setup)
+        setup
+        ;;
+    build)
+        build_app_as_dmg_installer
+        ;;
+    get-artifacts)
+        pass_artifact_to_job
+        ;;
+    *)
+        echo "Unknown stage '${1:-}'"
+        exit 1
+esac

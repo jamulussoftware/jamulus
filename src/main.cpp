95a65096c468193bcc8a731fd5f8fdcd0523fd47
pgScorpio <pg_Scorpio@hotmail.com>
2022-03-26 20:27:58 +0100

    Connection status issue jamulussoftware#2519 second stage
    Moved Connect/Disconnect code from CClientdlg to CClient.
    Now using the proper connected checks in several places.
    Added bDisconnectAndDisable to CChannel. (For a Client now
    Channel.Disconnect() will block audio data and auto disable the channel on disconnected)

--- src/main.cpp
+++ src/main.cpp
@@ -920,9 +920,6 @@ int main ( int argc, char** argv )
                 // initialise message boxes
                 CMsgBoxes::init ( &ClientDlg, strClientName.isEmpty() ? QString ( APP_NAME ) : QString ( APP_NAME ) + " " + strClientName );
 
-                // initialise message boxes
-                CMsgBoxes::init ( &ClientDlg, strClientName.isEmpty() ? QString ( APP_NAME ) : QString ( APP_NAME ) + " " + strClientName );
-
                 // show dialog
                 ClientDlg.show();
 

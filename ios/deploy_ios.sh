5da2236598369b8473cc5b88593e9548f2fa9768
Dan G <dan.garton@gmail.com>
2022-05-09 19:48:04 +0200

    sanitize

--- ios/deploy_ios.sh
+++ ios/deploy_ios.sh
@@ -2,7 +2,7 @@
 set -eu
 
 root_path="$(pwd)"
-project_path="${root_path}/Koord-RT.pro"
+project_path="${root_path}/Jamulus.pro"
 iosdeploy_path="${root_path}/ios"
 resources_path="${root_path}/src/res"
 build_path="${root_path}/build"
@@ -49,17 +49,17 @@ build_ipa()
     ## Builds an ipa file for iOS. Should be run from the repo-root
 
     # Create Xcode project file
-    qmake -spec macx-xcode Koord-RT.pro
+    qmake -spec macx-xcode Jamulus.pro
 
     # disable deprecation warnings re legacy build system - XCode 13 errors on this
-    /usr/libexec/PlistBuddy -c "Add :DisableBuildSystemDeprecationDiagnostic bool" Koord-RT.xcodeproj/project.xcworkspace/xcshareddata/WorkspaceSettings.xcsettings
-    /usr/libexec/PlistBuddy -c "Set :DisableBuildSystemDeprecationDiagnostic true" Koord-RT.xcodeproj/project.xcworkspace/xcshareddata/WorkspaceSettings.xcsettings
+    /usr/libexec/PlistBuddy -c "Add :DisableBuildSystemDeprecationDiagnostic bool" Jamulus.xcodeproj/project.xcworkspace/xcshareddata/WorkspaceSettings.xcsettings
+    /usr/libexec/PlistBuddy -c "Set :DisableBuildSystemDeprecationDiagnostic true" Jamulus.xcodeproj/project.xcworkspace/xcshareddata/WorkspaceSettings.xcsettings
 
     # Build
     if [[ -z "$iosdist_cert_name" ]]; then
         # Build unsigned
-        /usr/bin/xcodebuild -project Koord-RT.xcodeproj -scheme Koord-RT -configuration Release clean archive \
-            -archivePath "build/Koord-RT.xcarchive" \
+        /usr/bin/xcodebuild -project Jamulus.xcodeproj -scheme Jamulus -configuration Release clean archive \
+            -archivePath "build/Jamulus.xcarchive" \
             CODE_SIGN_IDENTITY="" \
             CODE_SIGNING_REQUIRED=NO \
             CODE_SIGNING_ALLOWED=NO \
@@ -68,23 +68,23 @@ build_ipa()
         # Build signed ipa file
         # Ref: https://developer.apple.com/forums/thread/70326
         # // Builds the app into an archive
-        /usr/bin/xcodebuild -project Koord-RT.xcodeproj -scheme Koord-RT -configuration Release clean archive \
-            -archivePath "build/Koord-RT.xcarchive" \
-            DEVELOPMENT_TEAM="TXZ4FR95HG" \
+        /usr/bin/xcodebuild -project Jamulus.xcodeproj -scheme Jamulus -configuration Release clean archive \
+            -archivePath "build/Jamulus.xcarchive" \
+            DEVELOPMENT_TEAM="XXXXXXXXXX" \
             CODE_SIGN_IDENTITY="" \
             CODE_SIGNING_REQUIRED=NO \
             CODE_SIGNING_ALLOWED=NO
 
         #FIXME this may be redundant - since provisioning profile is specified in exportOptionsRelease.plist
-        cp ~/Library/MobileDevice/Provisioning\ Profiles/embedded.mobileprovision build/Koord-RT.xcarchive/Products/Applications/Koord-RT.app/
+        cp ~/Library/MobileDevice/Provisioning\ Profiles/embedded.mobileprovision build/Jamulus.xcarchive/Products/Applications/Jamulus.app/
 
         # // Exports the archive according to the export options specified by the plist
-        # export signed installer to build/Exports/Koord-RT.ipa
+        # export signed installer to build/Exports/Jamulus.ipa
         /usr/bin/xcodebuild -exportArchive \
-            -archivePath "build/Koord-RT.xcarchive" \
+            -archivePath "build/Jamulus.xcarchive" \
             -exportPath  "build/Exports/" \
             -exportOptionsPlist "ios/exportOptionsRelease.plist" \
-            DEVELOPMENT_TEAM="TXZ4FR95HG" \
+            DEVELOPMENT_TEAM="XXXXXXXXXX" \
             CODE_SIGN_IDENTITY="${iosdist_cert_name}" \
             CODE_SIGNING_REQUIRED=YES \
             CODE_SIGNING_ALLOWED=YES \
@@ -94,16 +94,16 @@ build_ipa()
     # Generate unsigned ipa by copying the .app structure from the xcarchive directory
     cd ${root_path}
     mkdir -p build/unsigned/Payload
-    cp -r build/Koord-RT.xcarchive/Products/Applications/Koord-RT.app build/unsigned/Payload/
+    cp -r build/Jamulus.xcarchive/Products/Applications/Jamulus.app build/unsigned/Payload/
     cd build/unsigned
-    zip -0 -y -r Koord-RT.ipa Payload/
+    zip -0 -y -r Jamulus.ipa Payload/
 
     # copy files
     cd ${root_path}
     # unsigned IPA
-    mv build/unsigned/Koord-RT.ipa deploy/Koord-RT_unsigned.ipa
+    mv build/unsigned/Jamulus.ipa deploy/Jamulus_unsigned.ipa
     # signed IPA
-    mv build/Exports/Koord-RT.ipa deploy/Koord-RT_signed.ipa
+    mv build/Exports/Jamulus.ipa deploy/Jamulus_signed.ipa
 }
 
 # Cleanup previous deployments

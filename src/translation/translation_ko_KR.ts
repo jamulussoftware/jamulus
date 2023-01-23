8125df464ad57659d88b41d415e0559dcde9fdf3
이정희 <daemul72@gmail.com>
2023-01-23 04:19:20 +0000

    Translated using Weblate (Korean)
    Currently translated at 99.8% (604 of 605 strings)
    
    Translation: Jamulus/Jamulus app
    Translate-URL: https://hosted.weblate.org/projects/jamulus/jamulus-app/ko/

--- src/translation/translation_ko_KR.ts
+++ src/translation/translation_ko_KR.ts
@@ -119,7 +119,7 @@
     <message>
         <location filename="../util.cpp" line="745"/>
         <source>Norwegian Bokmål</source>
-        <translation type="unfinished"></translation>
+        <translation>노르웨이 부크몰어</translation>
     </message>
     <message>
         <location filename="../util.cpp" line="753"/>

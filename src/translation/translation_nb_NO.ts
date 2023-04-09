bc3afe3dd4525c2adc0677c1042eeff07080882c
Allan Nordhøy <epost@anotheragency.no>
2023-04-09 13:46:03 +0000

    Translated using Weblate (Norwegian Bokmål)
    Currently translated at 46.7% (283 of 605 strings)
    
    Translation: Jamulus/Jamulus app
    Translate-URL: https://hosted.weblate.org/projects/jamulus/jamulus-app/nb_NO/

--- src/translation/translation_nb_NO.ts
+++ src/translation/translation_nb_NO.ts
@@ -111,7 +111,7 @@
     <message>
         <location filename="../util.cpp" line="745"/>
         <source>Norwegian Bokmål</source>
-        <translation type="unfinished"></translation>
+        <translation>Norsk bokmål</translation>
     </message>
     <message>
         <location filename="../util.cpp" line="753"/>

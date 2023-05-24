3b6d500c0041f0c1d3c545d4473402a31bdbe9e2
ann0see <20726856+ann0see@users.noreply.github.com>
2023-05-24 21:03:56 +0200

    Pass qmake path by parameter and upgrade macOS

--- ios/deploy_ios.sh
+++ ios/deploy_ios.sh
@@ -1,10 +1,31 @@
 #!/bin/bash
 set -eu -o pipefail
 
+
+qmake_path=""
+
+while getopts 'hs:' flag; do
+    case "${flag}" in
+        m)
+            qmake_path=$OPTARG
+            if [[ -z "$qmake_path" ]]; then
+                echo "Please add the path to the qmake binary: -m \"</path/to/ios/qmake/binary>\""
+            fi
+            ;;
+        h)
+            echo "Usage: -m </path/to/ios/qmake/binary>"
+            exit 0
+            ;;
+        *)
+            exit 1
+            ;;
+    esac
+done
+
 ## Builds an ipa file for iOS. Should be run from the repo-root
 
 # Create Xcode file and build
-qmake -spec macx-xcode Jamulus.pro
+eval "${qmake_path} Jamulus.pro"
 /usr/bin/xcodebuild -project Jamulus.xcodeproj -scheme Jamulus -configuration Release clean archive -archivePath "build/Jamulus.xcarchive" CODE_SIGN_IDENTITY="" CODE_SIGNING_REQUIRED=NO CODE_SIGNING_ALLOWED=NO CODE_SIGN_ENTITLEMENTS=""
 
 # Generate ipa by copying the .app file from the xcarchive directory

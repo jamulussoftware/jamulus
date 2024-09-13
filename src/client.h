9f936196d1df078ec546b02025a14ad39f9754b7
pgScorpio <pg_Scorpio@hotmail.com>
2024-09-13 22:42:37 +0200

    Refactor Connect out to CClient
    This is an extract from https://github.com/jamulussoftware/jamulus/pull/2550
    Co-authored-by: ann0see <20726856+ann0see@users.noreply.github.com>

--- src/client.h
+++ src/client.h
@@ -110,7 +110,6 @@ class CClient : public QObject
 public:
     CClient ( const quint16  iPortNumber,
               const quint16  iQosNumber,
-              const QString& strConnOnStartupAddress,
               const QString& strMIDISetup,
               const bool     bNoAutoJackConnect,
               const QString& strNClientName,
@@ -121,6 +120,12 @@ public:
 
     void Start();
     void Stop();
+    void Connect ( QString strServerAddress, QString strServerName );
+
+    // The ConnectedServerName is emitted by Connected() to update the UI with a human readable server name
+    void    SetConnectedServerName ( const QString strServerName ) { strConnectedServerName = strServerName; };
+    QString GetConnectedServerName() const { return strConnectedServerName; };
+
     bool IsRunning() { return Sound.IsRunning(); }
     bool IsCallbackEntered() const { return Sound.IsCallbackEntered(); }
     bool SetServerAddr ( QString strNAddr );
@@ -287,6 +292,10 @@ protected:
     int  EvaluatePingMessage ( const int iMs );
     void CreateServerJitterBufferMessage();
 
+    // information for the connected server
+
+    QString strConnectedServerName;
+
     // only one channel is needed for client application
     CChannel  Channel;
     CProtocol ConnLessProtocol;
@@ -387,7 +396,8 @@ protected slots:
     {
         if ( InetAddr == Channel.GetAddress() )
         {
-            emit Disconnected();
+            // Stop client in case it received a Disconnection request
+            Stop();
         }
     }
     void OnCLPingReceived ( CHostAddress InetAddr, int iMs );
@@ -427,7 +437,11 @@ signals:
 
     void CLChannelLevelListReceived ( CHostAddress InetAddr, CVector<uint16_t> vecLevelList );
 
+    void Connected ( QString strServerName );
+    void ConnectingFailed ( QString errorMessage );
+    void DisconnectClient();
     void Disconnected();
+
     void SoundDeviceChanged ( QString strError );
     void ControllerInFaderLevel ( int iChannelIdx, int iValue );
     void ControllerInPanValue ( int iChannelIdx, int iValue );

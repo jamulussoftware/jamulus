75792ae932072d9a5ececd788824e83af1f69848
Tony Mountifield <tony@mountifield.org>
2024-11-20 17:43:24 +0000

    Fix for clang-format

--- src/clientdlg.cpp
+++ src/clientdlg.cpp
@@ -333,10 +333,8 @@ CClientDlg::CClientDlg ( CClient*         pNCliP,
     QAction* ByCityAction =
         pViewMenu->addAction ( tr ( "Sort Users by &City" ), this, SLOT ( OnSortChannelsByCity() ), QKeySequence ( Qt::CTRL + Qt::Key_T ) );
 
-    QAction* ByServerChannelAction = pViewMenu->addAction ( tr ( "Sort Users by Chann&el" ),
-                                                            this,
-                                                            SLOT ( OnSortChannelsByChannel() ),
-                                                            QKeySequence ( Qt::CTRL + Qt::Key_E ) );
+    QAction* ByServerChannelAction =
+        pViewMenu->addAction ( tr ( "Sort Users by Chann&el" ), this, SLOT ( OnSortChannelsByChannel() ), QKeySequence ( Qt::CTRL + Qt::Key_E ) );
 
     OwnFaderFirstAction->setCheckable ( true );
     OwnFaderFirstAction->setChecked ( pSettings->bOwnFaderFirst );

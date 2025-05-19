1fcaee4bed8b4e96312dfa868d5f008e8a8f444e
Manuela Silva <mmsrs@sky.com>
2025-05-19 15:41:26 +0200

    Translated using Weblate (Portuguese (Portugal))
    Currently translated at 100.0% (627 of 627 strings)
    
    Translation: Jamulus/Jamulus app
    Translate-URL: https://hosted.weblate.org/projects/jamulus/jamulus-app/pt_PT/

--- src/translation/translation_pt_PT.ts
+++ src/translation/translation_pt_PT.ts
@@ -34,7 +34,7 @@
     <message>
         <location filename="../util.cpp" line="392"/>
         <source>Qt cross-platform application framework</source>
-        <translation>Estrutura de aplicações multiplataforma Qt</translation>
+        <translation>Estrutura de aplicações multiplataforma do Qt</translation>
     </message>
     <message>
         <location filename="../util.cpp" line="392"/>
@@ -207,7 +207,7 @@
     <message>
         <location filename="../aboutdlgbase.ui" line="59"/>
         <source>TextLabelVersion</source>
-        <translation>TextoEtiquetaVersão</translation>
+        <translation>TextLabelVersion</translation>
     </message>
     <message>
         <source>Copyright (C) 2005-2025 Volker Fischer and others</source>
@@ -220,7 +220,7 @@
     <message>
         <location filename="../aboutdlgbase.ui" line="72"/>
         <source>Copyright © 2005-2025 The Jamulus Development Team</source>
-        <translation>Copyright © 2005-2025 A equipe de desenvolvimento Jamulus</translation>
+        <translation>Direitos de Autor © 2005-2025 A Equipa de Desenvolvimento do Jamulus</translation>
     </message>
     <message>
         <location filename="../aboutdlgbase.ui" line="106"/>
@@ -289,12 +289,12 @@
     <message>
         <location filename="../audiomixerboard.cpp" line="986"/>
         <source>T R Y I N G   T O   C O N N E C T</source>
-        <translation>T E N T A N D O   L I G A R</translation>
+        <translation>A  T E N T A R   L I G A R</translation>
     </message>
     <message>
         <location filename="../audiomixerboard.cpp" line="1193"/>
         <source>RECORDING ACTIVE</source>
-        <translation>GRAVAÇÃO ACTIVA</translation>
+        <translation>GRAVAÇÃO ATIVA</translation>
     </message>
     <message>
         <location filename="../audiomixerboard.cpp" line="1203"/>
@@ -316,7 +316,7 @@
     <message>
         <location filename="../audiomixerboard.cpp" line="142"/>
         <source>Input level of the current audio channel at the server</source>
-        <translation>Nível de entrada deste canal de áudio do servidor</translation>
+        <translation>Nível de entrada do canal de áudio atual no servidor</translation>
     </message>
     <message>
         <location filename="../audiomixerboard.cpp" line="145"/>
@@ -330,7 +330,7 @@
     <message>
         <location filename="../audiomixerboard.cpp" line="148"/>
         <source>Local mix level setting of the current audio channel at the server</source>
-        <translation>Configuração do nível de mistura local deste canal de áudio do servidor</translation>
+        <translation>Definição do nível de mistura local do canal de áudio atual no servidor</translation>
     </message>
     <message>
         <location filename="../audiomixerboard.cpp" line="151"/>
@@ -1232,7 +1232,7 @@ Nós silenciamos o seu canal e ativamos &apos;Silenciar-me&apos;. Por favor, res
     <message>
         <location filename="../clientdlgbase.ui" line="343"/>
         <source>Delay</source>
-        <translation>Latência</translation>
+        <translation>Atraso</translation>
     </message>
     <message>
         <source>Buffers</source>
@@ -1282,7 +1282,7 @@ Nós silenciamos o seu canal e ativamos &apos;Silenciar-me&apos;. Por favor, res
     <message>
         <location filename="../clientdlgbase.ui" line="512"/>
         <source>&amp;Chat</source>
-        <translation>Me&amp;nsagens</translation>
+        <translation>&amp;Conversação</translation>
     </message>
     <message>
         <location filename="../clientdlgbase.ui" line="531"/>
@@ -1346,7 +1346,7 @@ Nós silenciamos o seu canal e ativamos &apos;Silenciar-me&apos;. Por favor, res
     <message>
         <location filename="../clientsettingsdlg.cpp" line="87"/>
         <source>The jitter buffer setting is therefore a trade-off between audio quality and overall delay.</source>
-        <translation>A configuração do buffer de jitter é, portanto, uma troca entre a qualidade do áudio e o atraso geral.</translation>
+        <translation>A definição do buffer de jitter é, portanto, uma troca entre a qualidade do áudio e o atraso geral.</translation>
     </message>
     <message>
         <source>An auto setting of the jitter buffer size setting is available. If the check Auto is enabled, the jitter buffers of the local client and the remote server are set automatically based on measurements of the network and sound card timing jitter. If the Auto check is enabled, the jitter buffer size faders are disabled (they cannot be moved with the mouse).</source>
@@ -2966,7 +2966,7 @@ Nós silenciamos o seu canal e ativamos &apos;Silenciar-me&apos;. Por favor, res
     <message>
         <location filename="../connectdlg.cpp" line="101"/>
         <source>Click the button to clear the currently selected server address and delete it from the list of stored servers.</source>
-        <translation>Clique no botão para limpar o endereço de servidor selecionado e excluí-lo da lista de servidores armazenados.</translation>
+        <translation>Clique no botão para limpar o endereço do servidor selecionado e eliminá-lo da lista dos servidores guardados.</translation>
     </message>
     <message>
         <source>The IP address or URL of the server running the </source>
@@ -3089,7 +3089,7 @@ Nós silenciamos o seu canal e ativamos &apos;Silenciar-me&apos;. Por favor, res
     <message>
         <location filename="../connectdlgbase.ui" line="76"/>
         <source>Ping Time</source>
-        <translation>Latência</translation>
+        <translation>Tempo de Latência</translation>
     </message>
     <message>
         <location filename="../connectdlgbase.ui" line="81"/>
@@ -3954,7 +3954,7 @@ Nós silenciamos o seu canal e ativamos &apos;Silenciar-me&apos;. Por favor, res
         <location filename="../serverdlg.cpp" line="63"/>
         <source>&lt;b&gt;Directory:&lt;/b&gt; Select &apos;%1&apos; not to register your server with a directory.&lt;br&gt;Or select one of the genres to register with that directory.&lt;br&gt;Or select &apos;%2&apos; and specify a Custom Directory address on the Options tab to register with a custom directory.&lt;br&gt;&lt;br&gt;For any value except &apos;%1&apos;, this server registers with a directory so that a %3 user can select this server in the client connect dialog server list when they choose that directory.&lt;br&gt;&lt;br&gt;The registration of the server is renewed periodically to make sure that all servers in the connect dialog server list are actually available.</source>
         <comment>%1: directory type NONE; %2: directory type CUSTOM; %3 app name, Jamulus</comment>
-        <translation>&lt;b&gt;Diretoria:&lt;/b&gt; Selecione &apos;%1&apos; para não registar o seu servidor numa diretoria.&lt;br&gt;Ou selecione um dos géneros para registar com essa diretoria.&lt;br&gt;Ou selecione &apos;%2&apos; e especifique um endereço de Diretoria Personalizada no separador Opções para registar numa diretoria personalizada.&lt;br&gt;&lt;br&gt;Para qualquer valor exceto &quot;%1&quot;, este servidor regista numa diretoria para que um utilizador %3 possa selecionar este servidor na lista de servidores na janela da ligação do cliente quando escolher essa diretoria.&lt;br&gt;&lt;br&gt;O registo do servidor é renovado periodicamente para garantir que todos os servidores na lista de servidores da janela de ligação estão realmente disponíveis.</translation>
+        <translation>&lt;b&gt;Pasta:&lt;/b&gt; Selecione &apos;%1&apos; para não registar o seu servidor com uma pasta.&lt;br&gt;Ou selecione um dos géneros para registar com essa pasta.&lt;br&gt;Ou selecione &apos;%2&apos; e especifique um endereço da &quot;Diretoria Personalizada&quot; no separador das Opções para registar numa pasta personalizada.&lt;br&gt;&lt;br&gt;Para qualquer valor, exceto &quot;%1&quot;, este servidor regista numa pasta para que um utilizador %3 possa selecionar este servidor na lista de servidores na janela da ligação do cliente quando escolher essa pasta.&lt;br&gt;&lt;br&gt;O registo do servidor é renovado periodicamente para garantir que todos os servidores na lista de servidores da janela de ligação estão realmente disponíveis.</translation>
     </message>
     <message>
         <location filename="../serverdlg.cpp" line="133"/>
@@ -4212,7 +4212,7 @@ Nós silenciamos o seu canal e ativamos &apos;Silenciar-me&apos;. Por favor, res
     <message>
         <location filename="../util.h" line="648"/>
         <source>Check server version</source>
-        <translation>Verifique versão do servidor</translation>
+        <translation>Verifique a versão do servidor</translation>
     </message>
     <message>
         <location filename="../util.h" line="651"/>
@@ -4222,7 +4222,7 @@ Nós silenciamos o seu canal e ativamos &apos;Silenciar-me&apos;. Por favor, res
     <message>
         <location filename="../util.h" line="654"/>
         <source>Server list full at directory</source>
-        <translation>Lista de servidores cheia na diretoria</translation>
+        <translation>Lista dos servidores cheia na pasta</translation>
     </message>
     <message>
         <source>Directory server list full</source>
@@ -4263,7 +4263,7 @@ Nós silenciamos o seu canal e ativamos &apos;Silenciar-me&apos;. Por favor, res
         <location filename="../serverdlgbase.ui" line="39"/>
         <location filename="../serverdlgbase.ui" line="118"/>
         <source>Name</source>
-        <translation>Nome do Servidor</translation>
+        <translation>Nome</translation>
     </message>
     <message>
         <location filename="../serverdlgbase.ui" line="44"/>
@@ -4297,7 +4297,7 @@ Nós silenciamos o seu canal e ativamos &apos;Silenciar-me&apos;. Por favor, res
     <message>
         <location filename="../serverdlgbase.ui" line="206"/>
         <source>Chat Window Welcome (HTML/CSS Supported)</source>
-        <translation>Mensagem de Boas Vindas (HTML/CSS Suportado)</translation>
+        <translation>Boas-vindas da Janela de Conversação (HTML/CSS Suportado)</translation>
     </message>
     <message>
         <location filename="../serverdlgbase.ui" line="224"/>
@@ -4307,7 +4307,7 @@ Nós silenciamos o seu canal e ativamos &apos;Silenciar-me&apos;. Por favor, res
     <message>
         <location filename="../serverdlgbase.ui" line="274"/>
         <source>Custom Directory address</source>
-        <translation>End. de Diretório Personalizado</translation>
+        <translation>Endereço da Pasta Personalizada</translation>
     </message>
     <message>
         <location filename="../serverdlgbase.ui" line="288"/>
@@ -4331,7 +4331,7 @@ Nós silenciamos o seu canal e ativamos &apos;Silenciar-me&apos;. Por favor, res
     <message>
         <location filename="../serverdlgbase.ui" line="338"/>
         <source>Update check</source>
-        <translation>Verificação de actualização</translation>
+        <translation>Verificação de atualização</translation>
     </message>
     <message>
         <source>Make My Server Public (Register My Server in the Server List)</source>
@@ -4354,7 +4354,7 @@ Nós silenciamos o seu canal e ativamos &apos;Silenciar-me&apos;. Por favor, res
     <message>
         <location filename="../serverdlgbase.ui" line="253"/>
         <source>Recording Directory</source>
-        <translation>Diretório das Gravações</translation>
+        <translation>Pasta de Gravação</translation>
     </message>
     <message>
         <location filename="../serverdlgbase.ui" line="159"/>
@@ -4364,7 +4364,7 @@ Nós silenciamos o seu canal e ativamos &apos;Silenciar-me&apos;. Por favor, res
     <message>
         <location filename="../serverdlgbase.ui" line="75"/>
         <source>Directory</source>
-        <translation>Diretório</translation>
+        <translation>Pasta</translation>
     </message>
     <message>
         <location filename="../serverdlgbase.ui" line="166"/>
@@ -4375,7 +4375,7 @@ Nós silenciamos o seu canal e ativamos &apos;Silenciar-me&apos;. Por favor, res
         <location filename="../serverdlgbase.ui" line="232"/>
         <source>Language</source>
         <extracomment>Consider add &quot;(Lang)&quot; as a suffix to ensure that users who selected the wrong language find the correct button</extracomment>
-        <translation>Linguagem</translation>
+        <translation>Idioma</translation>
     </message>
     <message>
         <source>Directory Server Address:</source>
@@ -4384,7 +4384,7 @@ Nós silenciamos o seu canal e ativamos &apos;Silenciar-me&apos;. Por favor, res
     <message>
         <location filename="../serverdlgbase.ui" line="110"/>
         <source>My Server Info</source>
-        <translation>Informação do Servidor</translation>
+        <translation>Informação do Meu Servidor</translation>
     </message>
     <message>
         <location filename="../serverdlgbase.ui" line="125"/>
@@ -4417,22 +4417,22 @@ Nós silenciamos o seu canal e ativamos &apos;Silenciar-me&apos;. Por favor, res
     <message>
         <location filename="../serverlist.cpp" line="415"/>
         <source>Now a directory</source>
-        <translation>Agora um diretório</translation>
+        <translation>Agora uma pasta</translation>
     </message>
     <message>
         <location filename="../serverlist.cpp" line="421"/>
         <source>No longer a directory</source>
-        <translation>Deixou de ser um diretório</translation>
+        <translation>Deixou de ser uma pasta</translation>
     </message>
     <message>
         <location filename="../serverlist.cpp" line="780"/>
         <source>Could not open &apos;%1&apos; for read/write. Please check that %2 has permission (and that there is free space).</source>
-        <translation>Não foi possível abrir &apos;%1&apos; para leitura/escrita. Por favor, verifique se %2 tem permissão (e se há espaço livre).</translation>
+        <translation>Não foi possível abrir &apos;%1&apos; para ler/gravar. Por favor, verifique se %2 tem permissão (e se há espaço livre).</translation>
     </message>
     <message>
         <location filename="../serverlist.cpp" line="787"/>
         <source>Loading persistent server list file: %1</source>
-        <translation>A carregar ficheiro de lista de servidores persistentes: %1</translation>
+        <translation>A carregar o ficheiro da lista de servidores persistentes: %1</translation>
     </message>
     <message>
         <location filename="../serverlist.cpp" line="850"/>
@@ -4550,12 +4550,12 @@ Nós silenciamos o seu canal e ativamos &apos;Silenciar-me&apos;. Por favor, res
     <message>
         <location filename="../sound/asio/sound.cpp" line="57"/>
         <source>The selected audio device is no longer present in the system. Please check your audio device.</source>
-        <translation>O dispositivo de áudio actualmente seleccionado não se encontra presente no sistema. Verifique seu dispositivo de áudio.</translation>
+        <translation>O dispositivo de áudio atualmente selecionado já não se encontra presente no sistema. Por favor, verifique o seu dispositivo de áudio.</translation>
     </message>
     <message>
         <location filename="../sound/asio/sound.cpp" line="76"/>
         <source>Couldn&apos;t initialise the audio driver. Check if your audio hardware is plugged in and verify your driver settings.</source>
-        <translation>Não foi possível inicializar o driver de áudio. Confirme se seu equipamento de áudio está conectado e verifique suas configurações do driver.</translation>
+        <translation>Não foi possível inicializar o controlador de áudio. Confirme se o seu equipamento de áudio está ligado e verifique as suas definições do controlador.</translation>
     </message>
     <message>
         <location filename="../sound/asio/sound.cpp" line="146"/>

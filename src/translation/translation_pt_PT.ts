f3cd2f7571d083e3f161a0b26f910e220a85771b
Manuela Silva <mmsrs@sky.com>
2025-06-02 16:01:35 +0200

    Translated using Weblate (Portuguese (Portugal))
    Currently translated at 100.0% (627 of 627 strings)
    
    Translation: Jamulus/Jamulus app
    Translate-URL: https://hosted.weblate.org/projects/jamulus/jamulus-app/pt_PT/

--- src/translation/translation_pt_PT.ts
+++ src/translation/translation_pt_PT.ts
@@ -207,7 +207,7 @@
     <message>
         <location filename="../aboutdlgbase.ui" line="59"/>
         <source>TextLabelVersion</source>
-        <translation>TextLabelVersion</translation>
+        <translation>VersãoEtiquetaTexto</translation>
     </message>
     <message>
         <source>Copyright (C) 2005-2025 Volker Fischer and others</source>

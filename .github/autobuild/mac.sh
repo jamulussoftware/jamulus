042f43549f777503c609ee1d9011bfca46866d58
ann0see <20726856+ann0see@users.noreply.github.com>
2025-08-03 21:31:36 +0200

    Move Qt installation to ~/qt to re-enable caching

--- .github/autobuild/mac.sh
+++ .github/autobuild/mac.sh
@@ -26,7 +26,7 @@
 
 set -eu
 
-QT_DIR=/opt/qt
+QT_DIR=~/qt
 # The following version pinnings are semi-automatically checked for
 # updates. Verify .github/workflows/bump-dependencies.yaml when changing those manually:
 AQTINSTALL_VERSION=3.1.21

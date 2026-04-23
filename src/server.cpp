28cb2957920b5257a8d2c1bec652762d25dc5c03
Nils Brederlow <62596379+dingodoppelt@users.noreply.github.com>
2026-04-23 18:51:00 +0200

    here we go again, fix style

--- src/server.cpp
+++ src/server.cpp
@@ -874,7 +874,7 @@ void CServer::DecodeReceiveData ( const int iChanCnt, const int iNumClients )
                 return;
             }
 
-            const int  iOffset     = iB * SYSTEM_FRAME_SIZE_SAMPLES * vecNumAudioChannels[iChanCnt];
+            const int iOffset = iB * SYSTEM_FRAME_SIZE_SAMPLES * vecNumAudioChannels[iChanCnt];
             // Recognise a raw audio packet by its size
             // Greater or equal is used to account for iSndSndCrdFrameSizeFactor, which is not known to the server. Could modulo be used here insted?
             const bool bIsRawAudio = ( iCeltNumCodedBytes >= iClientFrameSizeSamples * vecNumAudioChannels[iChanCnt] * sizeof ( int16_t ) );

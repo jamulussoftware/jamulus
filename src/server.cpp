82d274461ff803f3684a3b81ce5c9f190c043f39
Nils Brederlow <62596379+dingodoppelt@users.noreply.github.com>
2026-06-18 20:01:35 +0200

    fix style
    (cherry picked from commit 1e1f54ab90c566dbda461cf40c4915e5680d7da1)

--- src/server.cpp
+++ src/server.cpp
@@ -1275,7 +1275,10 @@ void CServer::SendChatTextToAllConChannels ( const QString& strChatText )
     // Send chat text to all connected clients ---------------------------------
     for ( int i = 0; i < iMaxNumChannels; i++ )
     {
-        if ( vecChannels[i].IsConnected() ) vecChannels[i].CreateChatTextMes ( strChatText );
+        if ( vecChannels[i].IsConnected() )
+        {
+            vecChannels[i].CreateChatTextMes ( strChatText );
+        }
     }
 }
 

08e68fc8e5d097fa867f2a339c39cfc0a83ef5a5
Tony Mountifield <tony@mountifield.org>
2026-06-30 23:33:27 +0100

    Add some debug messages

--- src/client.cpp
+++ src/client.cpp
@@ -1158,7 +1158,7 @@ void CClient::OnClientIDReceived ( int iServerChanID )
     if ( bTcpSupported )
     {
         // *** Make TCP connection
-        qDebug() << Q_FUNC_INFO << "need to make TCP connection for" << iClientID;
+        qDebug() << Q_FUNC_INFO << "need to make TCP connection for client ID" << iClientID;
         ConnLessProtocol.CreateCLClientIDMes ( Channel.GetAddress(), iClientID, PROTO_TCP_LONG ); // create persistent TCP connection
     }
 
@@ -1208,6 +1208,7 @@ void CClient::OnCLTcpSupportedReceived ( CHostAddress InetAddr, int iID )
         {
             if ( pendingServerList.value ( InetAddr ) == CFM_UDP_REQUEST )
             {
+                qDebug() << "- UDP server list not received from" << InetAddr.toString() << "- retrying via TCP";
                 // request pending but reply not received - probably due to fragmentation drop
                 // re-request using TCP
                 pendingServerList.insert ( InetAddr, CFM_TCP_REQUEST );
@@ -1224,6 +1225,7 @@ void CClient::OnCLTcpSupportedReceived ( CHostAddress InetAddr, int iID )
         {
             if ( pendingClientList.value ( InetAddr ) == CFM_UDP_REQUEST )
             {
+                qDebug() << "- UDP client list not received from" << InetAddr.toString() << "- retrying via TCP";
                 // request pending but reply not received - probably due to fragmentation drop
                 // re-request using TCP
                 pendingClientList.insert ( InetAddr, CFM_TCP_REQUEST );
@@ -1242,7 +1244,7 @@ void CClient::OnCLTcpSupportedReceived ( CHostAddress InetAddr, int iID )
         if ( iClientID != INVALID_INDEX )
         {
             // *** Make TCP connection
-            qDebug() << Q_FUNC_INFO << "need to make TCP connection for" << iClientID;
+            qDebug() << Q_FUNC_INFO << "need to make TCP connection for client ID" << iClientID;
             Q_ASSERT ( InetAddr == Channel.GetAddress() );
             ConnLessProtocol.CreateCLClientIDMes ( InetAddr, iClientID, PROTO_TCP_LONG ); // create persistent TCP connection
         }

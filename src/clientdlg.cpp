0e577a40e65966b2465987af5c775c8330af3515
Tony Mountifield <tony@mountifield.org>
2026-07-28 17:59:28 +0100

    Empty pending hashes when opening connect dialog

--- src/clientdlg.cpp
+++ src/clientdlg.cpp
@@ -991,6 +991,8 @@ void CClientDlg::SetMyWindowTitle ( const int iNumClients )
 
 void CClientDlg::ShowConnectionSetupDialog()
 {
+    pClient->InitPendingLists(); // wipe slate clean for TCP support
+
     // show connect dialog
     bConnectDlgWasShown = true;
     ConnectDlg.show();

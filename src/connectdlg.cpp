bed1b45642ad5cf44c488c18aa0bb0a39f82b1aa
Tony Mountifield <tony@mountifield.org>
2026-08-08 15:44:02 +0100

    Ensure a server is always selected when list gains focus
    This fixes keyboard navigation when the list only contains one server.

--- src/connectdlg.cpp
+++ src/connectdlg.cpp
@@ -1175,6 +1175,11 @@ bool CConnectDlg::eventFilter ( QObject* obj, QEvent* event )
             lvwServers->setCurrentItem ( savedServer );
             savedServer = nullptr;
         }
+        else if ( lvwServers->topLevelItemCount() > 0 )
+        {
+            // no saved server: select first item in list for keyboard navigation
+            lvwServers->setCurrentItem ( lvwServers->topLevelItem ( 0 ) );
+        }
     }
 
     return QDialog::eventFilter ( obj, event );

78db36533a297efac050d68c4bee688096aacf80
Peter L Jones <peter.l.jones.dymlyd@gmail.com>
2026-09-05 16:44:15 +0100

    Externalise cache keys to prevent unneeded invalidation

---
+++ .github/autobuild/ios-dependencies.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# The following version pinnings are semi-automatically checked for updates.
+# Verify .github/workflows/bump-dependencies.yml when changing those manually:
+
+# Values are consumed by ios.sh and the dependency cache key.
+# shellcheck disable=SC2034
+AQTINSTALL_VERSION=3.3.0
+QT_VERSION=5.15.2
